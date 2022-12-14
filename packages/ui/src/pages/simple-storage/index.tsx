09d23e1d8eec3d6cbd9aa0d9b56e1e80b923a2af
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-12-14 12:41:36 +0100

    created styles for collapsed and expanded

--- packages/ui/src/pages/simple-storage/index.tsx
+++ packages/ui/src/pages/simple-storage/index.tsx
@@ -56,8 +56,11 @@ class SmartContract extends Component<
     this.setIsCollapsed(!this.state.isCollapsed);
   };
 
-  Collapsed = () => (
-    <form onSubmit={this.handleSubmit}>
+  Collapsed = (props: LayoutProps) => (
+    <form
+      onSubmit={this.handleSubmit}
+      className={props.className}
+    >
       <div id="write-function">
         <div id="input-clpsd">
           <button id="field">setNumber</button>
@@ -77,8 +80,70 @@ class SmartContract extends Component<
     </form>
   );
 
-  Expanded = () => (
-    <form onSubmit={this.handleSubmit}>
+  StyledCollapsed = styled(this.Collapsed)`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    #write-function:not(:first-child) {
+      padding-top: 0.5rem;
+    }
+    #write-function {
+      position: relative;
+      display: inline-flex;
+      width: 100%;
+      justify-content: space-between;
+      #input-clpsd:not(:last-child) {
+        padding: 0.3rem;
+      }
+      #input-clpsd {
+        * {
+          padding: 0.7rem;
+        }
+        width: 100%;
+        #field {
+          width: 30%;
+          background-color: #ef830f;
+          border-radius: 0.3rem 0 0 0.3rem;
+          color: #eef2f2;
+          border-color: #f3f1ef;
+          border-right: none;
+          border-left: 0.1rem solid;
+          border-top: 0.1rem solid;
+          border-bottom: 0.1rem solid;
+          font-size: 100%;
+        }
+        #field:hover  {
+          background-color: #e49a10;
+        }
+        #field:focus {
+          background-color: #faa609;
+        }
+        #field:active {
+          background-color: #f63513;
+        }
+        #argument {
+          width: 62%;
+          border: none;
+          border-radius: 0 0.3rem 0.3rem 0;
+        }
+      }
+      svg {
+        height: 24px;
+        width: 24px;
+      }
+      #expand-button {
+        padding: 0;
+        background-color: transparent;
+        border: none;
+      }
+    }
+  `;
+
+  Expanded = (props: LayoutProps) => (
+    <form
+      onSubmit={this.handleSubmit}
+      className={props.className}
+    >
       <div id="write-function-exp">
         <div id="title-exp">
           <p id="title-text">setNumber</p>
@@ -104,6 +169,66 @@ class SmartContract extends Component<
     </form>
   );
 
+  StyledExpanded = styled(this.Expanded)`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    #write-function-exp {
+      width: 100%;
+      height: 100%;
+      color: #617279;
+      #title-exp {
+        display: flex;
+        justify-content: space-between;
+        place-items: center;
+        padding-bottom: 0.5rem;
+        p {
+          padding-left: 0.5rem;
+        }
+        #title-text {
+          grid-area: title;
+        }
+        #expand-button {
+          padding: 0;
+          background-color: transparent;
+          border: none;
+        }
+        svg {
+          width: 24px;
+          height: 24px;
+        }
+      }
+      #input-exp {
+        display: flex;
+        justify-content: flex-end;
+        padding-bottom: 0.5rem;
+        * {
+          padding: 0.7rem;
+        }
+        #field-exp {
+          grid-area: smallTitle;
+        }
+        #argument-exp {
+          grid-area: input;
+        }
+      }
+      #button-exp {
+        display: flex;
+        justify-content: flex-end;
+        #transact-btn {
+          border: none;
+          padding-left: 1rem;
+          padding-right: 1rem;
+          font-size: 100%;
+          background-color: #ef830f;
+          padding: 0.7rem;
+          border-radius: 0.3rem;
+          color: #f3f1ef;
+        }
+      }
+    }
+  `;
+
   Layout = (props: LayoutProps) => (
     <div className={props.className}>
       <div id="area">{props.children}</div>
@@ -114,129 +239,6 @@ class SmartContract extends Component<
     #area {
       width: 25rem;
       padding: 2rem;
-      form {
-        width: 100%;
-        display: flex;
-        justify-content: space-between;
-
-        #write-function:not(:first-child) {
-          padding-top: 0.5rem;
-        }
-        #write-function {
-          position: relative;
-          display: inline-flex;
-          width: 100%;
-          justify-content: space-between;
-          #input-clpsd:not(:last-child) {
-            padding: 0.3rem;
-          }
-          #input-clpsd {
-            * {
-              padding: 0.7rem;
-            }
-            width: 100%;
-            #field {
-              width: 30%;
-              background-color: #ef830f;
-              border-radius: 0.3rem 0 0 0.3rem;
-              color: #eef2f2;
-              border-color: #f3f1ef;
-              border-right: none;
-              border-left: 0.1rem solid;
-              border-top: 0.1rem solid;
-              border-bottom: 0.1rem solid;
-              font-size: 100%;
-            }
-            #field:hover  {
-              background-color: #e49a10;
-            }
-            #field:focus {
-              background-color: #faa609;
-            }
-            #field:active {
-              background-color: #f63513;
-            }
-            #argument {
-              width: 62%;
-              border: none;
-              border-radius: 0 0.3rem 0.3rem 0;
-            }
-          }
-          svg {
-            height: 24px;
-            width: 24px;
-          }
-          #expand-button {
-            padding: 0;
-            background-color: transparent;
-            border: none;
-          }
-        }
-        #write-function-exp {
-          width: 100%;
-          height: 100%;
-          color: #617279;
-          #title-exp {
-            display: flex;
-            justify-content: space-between;
-            place-items: center;
-            padding-bottom: 0.5rem;
-            p {
-              padding-left: 0.5rem;
-            }
-            #title-text {
-              grid-area: title;
-            }
-            #expand-button {
-              padding: 0;
-              background-color: transparent;
-              border: none;
-            }
-            svg {
-              width: 24px;
-              height: 24px;
-            }
-          }
-          #input-exp {
-            display: flex;
-            justify-content: flex-end;
-            padding-bottom: 0.5rem;
-            * {
-              padding: 0.7rem;
-            }
-            #field-exp {
-              grid-area: smallTitle;
-            }
-            #argument-exp {
-              grid-area: input;
-            }
-          }
-          #button-exp {
-            display: flex;
-            justify-content: flex-end;
-            #transact-btn {
-              border: none;
-              padding-left: 1rem;
-              padding-right: 1rem;
-              font-size: 100%;
-              background-color: #ef830f;
-              padding: 0.7rem;
-              border-radius: 0.3rem;
-              color: #f3f1ef;
-            }
-          }
-        }
-      }
-      /* * {
-        padding-right: 2rem;
-        padding-left: 2rem;
-      }
-      &:first-child {
-        padding-top: 2rem;
-      }
-      &:last-child {
-        padding-bottom: 2rem;
-      } */
       border: 0.1rem solid;
       border-color: #344850;
       background-color: #052b3b;
@@ -250,9 +252,9 @@ class SmartContract extends Component<
     return (
       <this.Style>
         {this.state.isCollapsed ? (
-          <this.Collapsed />
+          <this.StyledCollapsed />
         ) : (
-          <this.Expanded />
+          <this.StyledExpanded />
         )}
       </this.Style>
     );
