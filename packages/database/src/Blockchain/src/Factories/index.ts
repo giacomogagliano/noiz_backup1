e3ba6f72be756786b47942baa099f25d46d9a9b6
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-12-14 18:20:20 +0100

    +

--- packages/database/src/Blockchain/src/Factories/index.ts
+++ packages/database/src/Blockchain/src/Factories/index.ts
@@ -1,6 +1,48 @@
+import { NoizContractFactory } from "../../../EVM";
+////
+import { ERC1155IndividualURI__factory } from "../types/contracts";
+import { ZERC1155TokenShop__factory } from "../types/contracts";
+import { Membership__factory } from "../types/contracts";
+import { ProvaMaster__factory } from "../types/contracts";
+import { SimpleStorage__factory } from "../types/contracts";
+import { USDC__factory } from "../types/contracts";
+////
+import erc1155IndUri from "./ERC1155IndividualUri.sol/ERC1155IndividualURI.json";
+import erc155TokenShop from "./ERC1155TokenShop.sol/ERC1155TokenShop.json";
+import membership from "./Membership.sol/Membership.json";
+import provaMaster from "./ProvaMaster.sol/ProvaMaster.json";
+import simpleStorage from "./SimpleStorage.sol/SimpleStorage.json";
+import usdc from "./USDC.sol/USDC.json";
+
 export * from "./getERC1155IndividualUri__factory";
 export * from "./getERC1155TokenShop__factory";
 export * from "./getMembership__factory";
 export * from "./getProvaMaster__factory";
 export * from "./getUSDC__factory";
 export * from "./getSimpleStorage__factory";
+
+export const erc1155IndividualURIFactory =
+  new NoizContractFactory<ERC1155IndividualURI__factory>(
+    erc1155IndUri
+  );
+
+export const erc1155TokenShopFactory =
+  new NoizContractFactory<ZERC1155TokenShop__factory>(
+    erc155TokenShop
+  );
+
+export const membershipFactory =
+  new NoizContractFactory<Membership__factory>(membership);
+
+export const provaMasterFactory =
+  new NoizContractFactory<ProvaMaster__factory>(
+    provaMaster
+  );
+
+export const simpleStorageFactory =
+  new NoizContractFactory<SimpleStorage__factory>(
+    simpleStorage
+  );
+
+export const usdcFactory =
+  new NoizContractFactory<USDC__factory>(usdc);
--- packages/database/src/EVM/Types/ZionContractFactories.ts
+++ packages/database/src/EVM/Types/ZionContractFactories.ts
@@ -1,12 +1,21 @@
 import { ethers, Signer } from "ethers";
 import { Factories, types } from "../../Blockchain";
 import {
+  erc1155IndividualURIFactory,
+  erc1155TokenShopFactory,
+  membershipFactory,
+  provaMasterFactory,
+  simpleStorageFactory,
+  usdcFactory,
+} from "../../Blockchain/src/Factories";
+import {
   Membership__factory,
   ProvaMaster__factory,
   USDC__factory,
   ZERC1155TokenShop__factory,
   SimpleStorage__factory,
 } from "../../Blockchain/src/types/contracts";
+import { NoizContractFactory } from "../classes";
 
 export type ERC1155IndividualURI_Factory =
   types.ZionContractsTypeScript.ERC1155IndividualURI__factory;
@@ -18,6 +27,26 @@ export type ContractFactory = ethers.ContractFactory;
 
 // TODO #38 mettere questi codici nelle cartelle delle classi non nei tipi
 export class ZionContractFactories {
+  newContractFactories: {
+    ERC1155IndividualURI: ERC1155IndividualURI_Factory;
+    ERC1155TokenShop: ZERC1155TokenShop__factory;
+    Membership: Membership__factory;
+    ProvaMaster: ProvaMaster__factory;
+    USDC: USDC__factory;
+    SimpleStorage: SimpleStorage__factory;
+    [key: string]: ContractFactory;
+  };
+
+  newNoizContractFactories: {
+    ERC1155IndividualURI: NoizContractFactory<ERC1155IndividualURI_Factory>;
+    ERC1155TokenShop: NoizContractFactory<ZERC1155TokenShop__factory>;
+    Membership: NoizContractFactory<Membership__factory>;
+    ProvaMaster: NoizContractFactory<ProvaMaster__factory>;
+    USDC: NoizContractFactory<USDC__factory>;
+    SimpleStorage: NoizContractFactory<SimpleStorage__factory>;
+    [key: string]: NoizContractFactory<ContractFactory>;
+  };
+
   contractFactories: {
     ERC1155IndividualURI: ERC1155IndividualURI_Factory;
     ERC1155TokenShop: ZERC1155TokenShop__factory;
@@ -45,6 +74,31 @@ export class ZionContractFactories {
       ProvaMaster,
       USDC,
       SimpleStorage,
+    };
+
+    this.newNoizContractFactories = {
+      ERC1155IndividualURI: erc1155IndividualURIFactory,
+      ERC1155TokenShop: erc1155TokenShopFactory,
+      Membership: membershipFactory,
+      ProvaMaster: provaMasterFactory,
+      SimpleStorage: simpleStorageFactory,
+      USDC: usdcFactory,
+    };
+
+    this.newContractFactories = {
+      ERC1155IndividualURI:
+        erc1155IndividualURIFactory.getContractFactory(
+          signer
+        ),
+      ERC1155TokenShop:
+        erc1155TokenShopFactory.getContractFactory(signer),
+      Membership:
+        membershipFactory.getContractFactory(signer),
+      ProvaMaster:
+        provaMasterFactory.getContractFactory(signer),
+      SimpleStorage:
+        simpleStorageFactory.getContractFactory(signer),
+      USDC: usdcFactory.getContractFactory(signer),
     };
   }
 }
