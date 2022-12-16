50470f35bab25e853763fed8260160d8dfaa094c
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-12-16 18:02:20 +0100

    Blockchain/deployERC1155 script

--- packages/database/src/Blockchain/scripts/deployERC1155.ts
+++ packages/database/src/Blockchain/scripts/deployERC1155.ts
@@ -3,28 +3,44 @@ import "@nomiclabs/hardhat-ethers";
 import { calculateTxCost } from "../src/lib/calculateTxCost";
 import { printDeployLog } from "../src/lib/printDeployLog";
 import { retrieveTxDatas } from "../src/lib/retrieveTxDatas";
-import type { erc1155IndividualUriSol } from "../typechain-types/zion/test";
-type ERC1155IndividualURI = erc1155IndividualUriSol.ERC1155IndividualURI;
+import type { erc1155IndividualUriSol } from "../src/types/contracts/contracts/zion/test";
+type ERC1155IndividualURI =
+  erc1155IndividualUriSol.ERC1155IndividualURI;
 
 const CONTRACTNAME = "ERC1155IndividualURI";
-const FILEPATH = "./contract-deployment-ERC1155IndividualURI.json";
+const FILEPATH =
+  "./contract-deployment-ERC1155IndividualURI.json";
 
 const DEPLOYMESS = "Deploying contracts with the account:";
 const BALANCEMESS = "Account balance:";
 const ADDMESS = "Token address: ";
-const SUCCESSMESS = "Contract deployed succesfully. Log reported in file: ";
+const SUCCESSMESS =
+  "Contract deployed succesfully. Log reported in file: ";
 
 async function main() {
   const [deployer] = await ethers.getSigners();
   const balanceInEth = await deployer.getBalance();
-  const balance = ethers.utils.formatEther(balanceInEth).toString();
+  const balance = ethers.utils
+    .formatEther(balanceInEth)
+    .toString();
   console.log(DEPLOYMESS + deployer.address);
   console.log(BALANCEMESS + balance);
-  const Contract = await ethers.getContractFactory(CONTRACTNAME);
-  const instance = (await Contract.deploy("")) as ERC1155IndividualURI;
+  const Contract = await ethers.getContractFactory(
+    CONTRACTNAME
+  );
+  const instance = (await Contract.deploy(
+    ""
+  )) as ERC1155IndividualURI;
   const data = retrieveTxDatas(instance.deployTransaction);
-  const { chainId, gasPrice, blockHash, blockNumber, from, hash, gasLimit } =
-    data;
+  const {
+    chainId,
+    gasPrice,
+    blockHash,
+    blockNumber,
+    from,
+    hash,
+    gasLimit,
+  } = data;
   const txCost = calculateTxCost(gasLimit, gasPrice);
   const log = printDeployLog(
     FILEPATH,
@@ -47,7 +63,7 @@ async function main() {
 
 main()
   .then(() => process.exit(0))
-  .catch((error) => {
+  .catch(error => {
     console.error(error);
     process.exit(1);
   });
