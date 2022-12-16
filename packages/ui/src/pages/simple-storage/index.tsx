c173f4b292eb669b2bbd5fd4609bd376f0ba264f
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-12-16 11:44:04 +0100

    +

--- packages/ui/src/pages/simple-storage/index.tsx
+++ packages/ui/src/pages/simple-storage/index.tsx
@@ -3,26 +3,50 @@ import styled from "styled-components";
 
 interface SimpleStorageProps {
   myString: string;
+  number: number;
   className?: string;
 }
 
 class SimpleStorage extends Component<SimpleStorageProps> {
-  MyString = ({ myString }: { myString: string }) => (
-    <div id="my-string">
-      <button onClick={this.handleMyStringBtn}>
-        myString
+  handleGetterOnClick = (
+    e: MouseEvent<HTMLButtonElement>
+  ) => {
+    e.preventDefault();
+    console.log("clicked my-string-btn");
+  };
+
+  Getter = ({
+    value,
+    id,
+    buttonMsg,
+  }: {
+    value: string | number;
+    id: string;
+    buttonMsg: string;
+  }) => (
+    <div id={id}>
+      <button onClick={this.handleGetterOnClick}>
+        {buttonMsg}
       </button>
-      <p>{myString}</p>
+      <p>{value}</p>
     </div>
   );
 
-  SetString = () => {
+  Input = ({
+    id,
+    type,
+    placeholder,
+  }: {
+    id: string;
+    type: "text" | "number";
+    placeholder: string;
+  }) => {
     return (
-      <div id="set-string">
-        <form id="set-string-form">
+      <div id={id}>
+        <form id={`${id}-form`}>
           <input
-            type="text"
-            placeholder="myString"
+            type={type}
+            placeholder={placeholder}
           ></input>
           <button type="submit">transact</button>
         </form>
@@ -30,17 +54,43 @@ class SimpleStorage extends Component<SimpleStorageProps> {
     );
   };
 
+  handleGetNumber = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    console.log("clicked get-number-btn");
+  };
+
   Layout = ({
     myString,
+    number,
     className,
   }: SimpleStorageProps) => {
-    const MyString = this.MyString;
-    const SetString = this.SetString;
+    const MyString = this.Getter;
+    const SetString = this.Input;
+    const GetNumber = this.Getter;
+    const SetNumber = this.Input;
     return (
       <div className={className}>
         <h1>Simple Storage</h1>
-        <MyString myString={myString} />
-        <SetString />
+        <MyString
+          value={myString}
+          id="my-string"
+          buttonMsg="myString"
+        />
+        <SetString
+          id="set-string"
+          placeholder="setString"
+          type="text"
+        />
+        <GetNumber
+          value={number}
+          buttonMsg="Get Number"
+          id="get-number"
+        />
+        <SetNumber
+          id="set-number"
+          placeholder="setNumber"
+          type="number"
+        />
       </div>
     );
   };
@@ -59,18 +109,25 @@ class SimpleStorage extends Component<SimpleStorageProps> {
       #set-string-form {
         display: inline-flex;
         button {
-          padding-left: 0.3rem;
+          margin-left: 0.3rem;
+        }
+      }
+    }
+    #get-number {
+      display: inline-flex;
+      p  {
+        padding-left: 0.3rem;
+      }
+    }
+    #set-number {
+      #set-number-form {
+        display: inline-flex;
+        button {
+          margin-left: 0.3rem;
         }
       }
     }
   `;
-
-  handleMyStringBtn = (
-    e: MouseEvent<HTMLButtonElement>
-  ) => {
-    e.preventDefault();
-    console.log("clicked myString btn");
-  };
 
   render() {
     const Element = this.StyledLayout;
@@ -83,15 +140,18 @@ export function getStaticProps(): {
 } {
   return {
     props: {
-      data: { myString: "i will show the string" },
+      data: {
+        myString: "i will show the string",
+        number: 10,
+      },
     },
   };
 }
 
 export default function index({
-  data: { myString },
+  data,
 }: {
   data: SimpleStorageProps;
 }) {
-  return <SimpleStorage myString={myString} />;
+  return <SimpleStorage {...data} />;
 }
