0e7910569d40746b293f69de4e9723bbebd81d5f
ariannatnl <arianna.minucci@me.com>
2022-12-18 22:44:19 +0100

    pagine P E PATH

---
+++ packages/ui/src/pages/styles/html-elements/p/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { P } from "../../../../HTML/React";
+
+export default function index() {
+  return <P>LOLOLOL</P>;
+}
--- packages/ui/src/pages/styles/html-elements/path/index.tsx
+++ packages/ui/src/pages/styles/html-elements/path/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { P } from "../../../../HTML/React";
+import { Path } from "../../../../HTML/React";
 
 export default function index() {
-  return <P>LOLOLOL</P>;
+  return <Path>LOLOLOL</Path>;
 }
