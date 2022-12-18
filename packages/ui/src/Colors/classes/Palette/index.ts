29826c7a2b7c10cefb61628bbd3d88d41da26187
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-12-18 23:49:36 +0100

    updated exports

--- packages/ui/src/Colors/classes/Palette/index.ts
+++ packages/ui/src/Colors/classes/Palette/index.ts
@@ -1,4 +1,5 @@
-import { Palette_v2 as v2, Palette_v2Props as v2Props } from './Palette_v2';
+export { Palette_v2 } from "./Palette_v2";
+export type { Palette_v2Props } from "./Palette_v2";
 export { Palette_v1 as Palette } from "./Palette_v1";
 import type { Palette_v1 as v1Props } from "./Palette_v1";
 ///// EXPORT
@@ -22,6 +23,3 @@ export * from "./Fluid3";
 
 //// Rainbow
 export * from "./Rainbow";
-
-export const Palette_v2 = v2;
-export type Palette_v2Props = v2Props;
