ffd5121eab509f8d05e2eca1bcd82d140f07cea5
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-12-19 17:45:12 +0100

    cambiati export

--- packages/database/src/EVM/classes/EVMweb/index.ts
+++ packages/database/src/EVM/classes/EVMweb/index.ts
@@ -1,6 +1,7 @@
-export { EVMweb_v2 } from "./EVMweb_v2";
+export { EVMweb_v2 as EVMweb } from "./EVMweb_v2";
+export type { IEVMweb_v2 as IEVMweb } from "./EVMweb_v2";
 ///// EXPORT
-export { EVMweb_v1 as EVMweb } from "./EVMweb_v1";
-export type { IEVMweb } from "./EVMweb_v1";
+export { EVMweb_v1 } from "./EVMweb_v1";
+export type { IEVMweb as IEVMweb_v1 } from "./EVMweb_v1";
 export type { RequireOnlyOne } from "./EVMweb_v1";
 ////////////
