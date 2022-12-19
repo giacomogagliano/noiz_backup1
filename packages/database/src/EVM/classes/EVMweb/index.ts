98d32339cc01bb09b92dabf0fb845b3a2280cad0
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-12-19 16:53:48 +0100

    aggiunto export di IEVMweb

--- packages/database/src/EVM/classes/EVMweb/index.ts
+++ packages/database/src/EVM/classes/EVMweb/index.ts
@@ -1,8 +1,6 @@
-import { EVMweb_v2 as v2, EVMweb_v2Props as v2Props } from './EVMweb_v2';
+export { EVMweb_v2 } from "./EVMweb_v2";
 ///// EXPORT
 export { EVMweb_v1 as EVMweb } from "./EVMweb_v1";
+export type { IEVMweb } from "./EVMweb_v1";
 export type { RequireOnlyOne } from "./EVMweb_v1";
 ////////////
-
-export const EVMweb_v2 = v2;
-export type EVMweb_v2Props = v2Props;
--- packages/database/src/EVM/classes/index.ts
+++ packages/database/src/EVM/classes/index.ts
@@ -5,6 +5,7 @@ export {};
 
 //// EVMweb
 export * from "./EVMweb";
+export type { IEVMweb } from "./EVMweb";
 
 //// EVM
 export * from "./EVM";
