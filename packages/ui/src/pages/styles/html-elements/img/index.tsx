c7f7f7ced9a253adcfb3753f61b6e9bb615c1dd5
ariannatnl <arianna.minucci@me.com>
2022-12-19 15:23:59 +0100

    added IMG url

--- packages/ui/src/pages/styles/html-elements/img/index.tsx
+++ packages/ui/src/pages/styles/html-elements/img/index.tsx
@@ -2,5 +2,7 @@ import React from "react";
 import { Img } from "../../../../HTML";
 
 export default function index() {
-  return <Img>LOLOLOL</Img>;
+  return (
+    <Img src="https://ipfs.io/ipfs/QmPGGowQG4oPoRf884Hz9WXivACoDni1GsdDYkRdXVLfJc?filename=QmPGGowQG4oPoRf884Hz9WXivACoDni1GsdDYkRdXVLfJc"></Img>
+  );
 }
