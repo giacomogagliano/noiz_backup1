a7e7706aa3c53b6efa0688c6c129477e8e549233
ariannatnl <arianna.minucci@me.com>
2022-12-20 12:41:41 +0100

    added Canvas infos in page

--- packages/ui/src/pages/styles/html-elements/canvas/index.tsx
+++ packages/ui/src/pages/styles/html-elements/canvas/index.tsx
@@ -1,10 +1,27 @@
 import React from "react";
-import { Div, Canvas } from "../../../../HTML";
+import {
+  Div,
+  Canvas,
+  H1,
+  P,
+  Code,
+} from "../../../../HTML";
 
 export default function index() {
   return (
     <Div>
-      <h1>The canvas element</h1>
+      <H1>The canvas element</H1>
+      <P>
+        The <Code>canvas</Code> tag is used to draw
+        graphics, on the fly, via scripting (usually
+        JavaScript). The <Code>canvas</Code> tag is
+        transparent, and is only a container for graphics,
+        you must use a script to actually draw the
+        graphics. Any text inside the <Code>canvas</Code>{" "}
+        element will be displayed in browsers with
+        JavaScript disabled and in browsers that do not
+        support <Code>canvas</Code>.
+      </P>
 
       <Canvas id="myCanvas">
         Your browser does not support the Canvas tag.
