235688de70104935af8327839eb92deb77287006
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-12-22 20:34:38 +0100

    importedd Propaganda_presale

--- packages/database/src/EVM/classes/NoizContractFactories/NoizContractFactories_v1.ts
+++ packages/database/src/EVM/classes/NoizContractFactories/NoizContractFactories_v1.ts
@@ -8,6 +8,7 @@ import {
   provaMasterFactory,
   simpleStorageFactory,
   usdcFactory,
+  propagandaPresaleFactory,
 } from "../../../Blockchain/src/Factories";
 import {
   Membership__factory,
@@ -15,6 +16,7 @@ import {
   USDC__factory,
   ZERC1155TokenShop__factory,
   SimpleStorage__factory,
+  Propaganda_Presale__factory,
 } from "../../../Blockchain/src/types/contracts";
 
 export interface INoizContractFactories_v1 {
@@ -44,6 +46,7 @@ export class NoizContractFactories_v1 {
     ProvaMaster: NoizContractFactory<ProvaMaster__factory>;
     USDC: NoizContractFactory<USDC__factory>;
     SimpleStorage: NoizContractFactory<SimpleStorage__factory>;
+    Propaganda_presale: NoizContractFactory<Propaganda_Presale__factory>;
     [key: string]: NoizContractFactory<ContractFactory>;
   };
 
@@ -54,6 +57,7 @@ export class NoizContractFactories_v1 {
     ProvaMaster: ProvaMaster__factory;
     USDC: USDC__factory;
     SimpleStorage: SimpleStorage__factory;
+    Propaganda_presale: Propaganda_Presale__factory;
     [key: string]: ContractFactory;
   };
   constructor(public signer?: Signer) {
@@ -66,6 +70,7 @@ export class NoizContractFactories_v1 {
       ProvaMaster: provaMasterFactory,
       SimpleStorage: simpleStorageFactory,
       USDC: usdcFactory,
+      Propaganda_presale: propagandaPresaleFactory,
     };
 
     this.contractFactories = {
@@ -82,6 +87,10 @@ export class NoizContractFactories_v1 {
       SimpleStorage:
         simpleStorageFactory.getContractFactory(signer),
       USDC: usdcFactory.getContractFactory(signer),
+      Propaganda_presale:
+        propagandaPresaleFactory.getContractFactory(
+          signer
+        ),
     };
   }
 }
