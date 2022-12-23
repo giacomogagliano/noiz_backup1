48f65bdbd6ade41ff1015edb1018340774c53c3e
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-12-23 01:25:24 +0100

    passed evm to Contract

--- packages/ui/src/pages/classes/pages/contract/index.tsx
+++ packages/ui/src/pages/classes/pages/contract/index.tsx
@@ -1,7 +1,8 @@
+import { EVMweb } from "@zionstate/database/EVM";
 import React from "react";
 import { Contract } from "../../../../HTML/React";
 
-export default function index() {
+export default function index({ evm }: { evm: EVMweb }) {
   return (
     <Contract
       contractAddr="0x121f1241f..14fa"
@@ -9,6 +10,7 @@ export default function index() {
       owner="0x03fd0..3bd5"
       price={Math.round((35 / 50) * 100) / 100}
       currency="USDC"
+      evm={evm}
     ></Contract>
   );
 }
