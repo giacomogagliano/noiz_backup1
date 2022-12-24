fe2bd941c1ef5c6d696c80d1d7b6a7e5b65ddec4
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-12-24 21:43:36 +0100

    todo and create issue

--- packages/zionbase/src/utils/node/util/ZionUtils/guard/makeErrGuard/makeErrGuard_v1.ts
+++ packages/zionbase/src/utils/node/util/ZionUtils/guard/makeErrGuard/makeErrGuard_v1.ts
@@ -2,12 +2,10 @@ export interface ImakeErrGuard_v1 {
   <T>(data: T): NonNullable<T>;
 }
 
-// TODO @ariannatnl mettere queste funzioni in moduli in
-// questa cartella
 export const makeErrGuard_v1: ImakeErrGuard_v1 = function (
   data
 ) {
-  // TODO @giacomogagliano aggiungere un modo per
+  // TODO #153 @giacomogagliano aggiungere un modo per
   // identificare il nome del paramentro, o ad esempio
   // convertire il tipo che arriva dal dato passato in
   // string per il messaggio di errore
