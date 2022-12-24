6f0256ed91cd561cbf72758ce6e702047f3da077
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-12-24 21:35:11 +0100

    todo and create issue

--- packages/database/src/FS/lib/search/filterZion/filterZion_v1.ts
+++ packages/database/src/FS/lib/search/filterZion/filterZion_v1.ts
@@ -8,14 +8,14 @@ export function filterZion_v1<
   }
 >(deps: [string, string][], obj: T, field: keyof T) {
   deps
-    .filter((dep) => dep[0][0] === "@")
-    .filter((dep) => dep[0][1] === "z")
-    .filter((dep) => dep[0][2] === "i")
-    .filter((dep) => dep[0][3] === "o")
-    .filter((dep) => dep[0][4] === "n")
-    .map((arr) => arr[0])
-    // TODO trovare solution typescript
+    .filter(dep => dep[0][0] === "@")
+    .filter(dep => dep[0][1] === "z")
+    .filter(dep => dep[0][2] === "i")
+    .filter(dep => dep[0][3] === "o")
+    .filter(dep => dep[0][4] === "n")
+    .map(arr => arr[0])
+    // TODO #186 @giacomogagliano trovare solution typescript
     // @ts-expect-error
-    .forEach((el) => (obj[field][el] = "*"));
+    .forEach(el => (obj[field][el] = "*"));
   return obj;
 }
