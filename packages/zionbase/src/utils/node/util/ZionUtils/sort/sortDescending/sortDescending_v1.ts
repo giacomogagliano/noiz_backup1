59f4b2ec812df45b73f4d23b7e45f7e3ba26a655
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-12-24 21:43:54 +0100

    del todo

--- packages/zionbase/src/utils/node/util/ZionUtils/sort/sortDescending/sortDescending_v1.ts
+++ packages/zionbase/src/utils/node/util/ZionUtils/sort/sortDescending/sortDescending_v1.ts
@@ -1,5 +1,7 @@
 export interface IsortDescending_v1 {
-  <T extends []>(a: T, b: T, index: number): number | undefined;
+  <T extends []>(a: T, b: T, index: number):
+    | number
+    | undefined;
 }
 
 /**
@@ -9,12 +11,17 @@ export interface IsortDescending_v1 {
  * @param index
  * @returns
  */
-export const sortDescending_v1: IsortDescending_v1 = function sortDescending<
-  T extends []
->(a: T, b: T, index: number): number | undefined {
-  if (typeof a[index] !== "number" && typeof b[index] !== "number") return;
-
-  // TODO errore TS
-  // @ts-expect-error
-  return b[index] - a[index];
-};
+export const sortDescending_v1: IsortDescending_v1 =
+  function sortDescending<T extends any[]>(
+    a: T,
+    b: T,
+    index: number
+  ): number | undefined {
+    if (
+      typeof a[index] !== "number" &&
+      typeof b[index] !== "number"
+    ) {
+      return;
+    }
+    return b[index] - a[index];
+  };
