8b7050afd6d0ac50a4f4dd02c1ac504e9eba5142
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-12-24 16:05:35 +0100

    create content

--- packages/ui/src/pages/classes/md/md-react/index.tsx
+++ packages/ui/src/pages/classes/md/md-react/index.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Md } from "../../../../HTML/React/classes";
+import { readAndParse } from "../../../../library";
 
-export default function index() {
-  return (
-    <div>index</div>
-  )
+export function getStaticProps() {
+  const res = readAndParse("./posts", "Propaganda");
+  return { props: { data: { post: res } } };
+}
+
+export default function index({
+  data: { post },
+}: {
+  data: { post: string };
+}) {
+  return <Md md_react contentString={post} article></Md>;
 }
