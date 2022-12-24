7a3089f3aa8b3c9450b28efca8aa5e4cb1a96ce7
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-12-24 16:31:56 +0100

    create Test

--- packages/ui/src/pages/classes/md/md-raw_react/index.tsx
+++ packages/ui/src/pages/classes/md/md-raw_react/index.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Md } from "../../../../HTML/React/classes";
+import { readAndParse } from "../../../../library";
 
-export default function index() {
+export function getStaticProps() {
+  const res = readAndParse("./posts", "Propaganda");
+  return { props: { data: { post: res } } };
+}
+
+export default function index({
+  data: { post },
+}: {
+  data: { post: string };
+}) {
   return (
-    <div>index</div>
-  )
+    <Md md_raw_react contentString={post} article></Md>
+  );
 }
