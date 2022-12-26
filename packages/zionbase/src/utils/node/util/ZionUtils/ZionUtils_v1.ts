634df8be4e35b7b3f8762299c5bc9288d2c54a5e
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-12-26 17:21:44 +0100

    create export add to ZionUtils and test: stringifyBase64

--- packages/zionbase/src/utils/node/util/ZionUtils/ZionUtils_v1.ts
+++ packages/zionbase/src/utils/node/util/ZionUtils/ZionUtils_v1.ts
@@ -20,6 +20,7 @@ import {
   sliceArray,
   subtractArrays,
   sortDescending,
+  stringifyBase64,
   // joinPaths,
   // buildPathTuple,
   // buildPaths,
@@ -39,34 +40,64 @@ export interface IZionUtils_v1 {
   inspect(obj: IInspectArguments): string;
   debuglog(
     section: string,
-    callback?: ((fn: util.DebugLoggerFunction) => void) | undefined
+    callback?:
+      | ((fn: util.DebugLoggerFunction) => void)
+      | undefined
   ): util.DebugLogger;
   promisify: <TCustom extends Function>(
     fn: util.CustomPromisify<TCustom>
   ) => TCustom;
   ////// zion
   // ARRAY
-  checkArraysContent: <T>(array: T[], nextArray: T[]) => boolean;
-  checkArrayElementsConstructor<T>(array: T[], constructor: Function): boolean;
-  checkObjectConstructor(object: object, constructor: Function): boolean;
-  hasArrayObjectElements(array: object[]): boolean | string;
+  checkArraysContent: <T>(
+    array: T[],
+    nextArray: T[]
+  ) => boolean;
+  checkArrayElementsConstructor<T>(
+    array: T[],
+    constructor: Function
+  ): boolean;
+  checkObjectConstructor(
+    object: object,
+    constructor: Function
+  ): boolean;
+  hasArrayObjectElements(
+    array: object[]
+  ): boolean | string;
   isArrayEmpty(array?: any[]): boolean;
-  changePosition<T>(array: T[], old: number, new_pos: number): T[] | string;
-  extractSameElementsFromArray<T extends string | boolean | number>(
+  changePosition<T>(
+    array: T[],
+    old: number,
+    new_pos: number
+  ): T[] | string;
+  extractSameElementsFromArray<
+    T extends string | boolean | number
+  >(
     array1: T[],
     array2: T[]
   ): T[];
   popFirst<T>(array: T[]): T[];
-  removeSpaceFromString(type: number, string: string): string;
+  removeSpaceFromString(
+    type: number,
+    string: string
+  ): string;
   sliceArray<T>(size: number, array: T): T[][] | string;
   subtractArrays(arr1: string[], arr2: string[]): string[];
   // STRINGS
   splitAt(string: string, symbol: string): string[];
   upperCaseFirst(string: string): string;
+  stringifyBase64(this: this): string;
   // SORTING
-  sortDescending<T>(a: T[], b: T[], index: number): number | undefined;
+  sortDescending<T>(
+    a: T[],
+    b: T[],
+    index: number
+  ): number | undefined;
   // CALLBACK
-  chiamaNVolteCallback<T>(volte: number, callback: Function): T[];
+  chiamaNVolteCallback<T>(
+    volte: number,
+    callback: Function
+  ): T[];
   // MATH
   quantiDecimaliDopoLaVirgola(number: number): number;
   massimoComuneDivisore(a: number, b: number): number;
@@ -74,8 +105,14 @@ export interface IZionUtils_v1 {
   convertDecimalToFractionString(decimale: number): string; //////////FATTO
   // PATHS
   joinPaths(...paths: string[]): string;
-  buildPathTuple(paths: string[], text: string): [string, string];
-  buildPaths(path: string, array: string[]): [string, string][];
+  buildPathTuple(
+    paths: string[],
+    text: string
+  ): [string, string];
+  buildPaths(
+    path: string,
+    array: string[]
+  ): [string, string][];
 }
 
 export class ZionUtils_v1 implements IZionUtils_v1 {
@@ -90,12 +127,14 @@ export class ZionUtils_v1 implements IZionUtils_v1 {
   ////// zion
   ////// arrays
   checkArraysContent = checkArraysContent;
-  checkArrayElementsConstructor = checkArrayElementsConstructor;
+  checkArrayElementsConstructor =
+    checkArrayElementsConstructor;
   checkObjectConstructor = checkObjectConstructor;
   hasArrayObjectElements = hasArrayObjectElements;
   isArrayEmpty = isArrayEmpty;
   changePosition = changePosition;
-  extractSameElementsFromArray = extractSameElementsFromArray;
+  extractSameElementsFromArray =
+    extractSameElementsFromArray;
   popFirst = popFirst;
   removeSpaceFromString = removeSpaceFromString;
   sliceArray = sliceArray;
@@ -103,21 +142,27 @@ export class ZionUtils_v1 implements IZionUtils_v1 {
   // strings
   splitAt = splitAt;
   upperCaseFirst = upperCaseFirst;
+  stringifyBase64 = stringifyBase64;
   // sorting
   sortDescending = sortDescending;
   // callback
   chiamaNVolteCallback = chiamaNVolteCallback;
   // maths
-  quantiDecimaliDopoLaVirgola = quantiDecimaliDopoLaVirgola;
+  quantiDecimaliDopoLaVirgola =
+    quantiDecimaliDopoLaVirgola;
   massimoComuneDivisore = massimoComuneDivisore;
   modulo = modulo;
-  convertDecimalToFractionString = convertDecimalToFractionString;
+  convertDecimalToFractionString =
+    convertDecimalToFractionString;
   // paths
   joinPaths(...paths: string[]) {
     return join(...paths);
   }
   buildPathTuple(paths: string[], text: string) {
-    let res: [string, string] = [this.joinPaths.bind(this)(...paths), text];
+    let res: [string, string] = [
+      this.joinPaths.bind(this)(...paths),
+      text,
+    ];
     return res;
   }
   buildPaths(path: string, array: string[]) {
