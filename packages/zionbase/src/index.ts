d7ea4676ccb40292f4f25561cc9147cd2b10e73f
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-12-26 22:50:09 +0100

    fix @giacomogagliano check all files which import zionbase in ui #238

--- packages/zionbase/src/index.ts
+++ packages/zionbase/src/index.ts
@@ -1,5 +1,3 @@
 // reexporting zionbase in the root for backward compatibility
-// TODO #238 @giacomogagliano check all files which import zionbase in ui
-export * from "./zionbase";
 export * as utils from "./utils";
 export * as zionbase from "./zionbase";
