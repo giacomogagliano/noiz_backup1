b764aed2f4192c45bde9ff5f4c09e2945bb3ce2e
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-12-26 01:49:42 +0100

    export guard from /utils

--- packages/zionbase/src/utils/node/index.ts
+++ packages/zionbase/src/utils/node/index.ts
@@ -1,4 +1,4 @@
 export * as util from "./util";
-export { swtch, dataGuard } from "./util";
+export { swtch, dataGuard, guard } from "./util";
 export * as process from "./process";
 export * as fetch from "./fetch";
