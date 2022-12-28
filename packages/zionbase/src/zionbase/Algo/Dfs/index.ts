1d7aa9fe0051b724087fa3db7254b6b192cbdeb8
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-12-28 20:20:23 +0100

    create
    add
    
    test

--- packages/zionbase/src/zionbase/Algo/Dfs/index.ts
+++ packages/zionbase/src/zionbase/Algo/Dfs/index.ts
@@ -2,3 +2,5 @@
 export { Dfs_v1 as Dfs } from "./Dfs_v1";
 export type { Dfs_v1Ctor as DfsCtor } from "./Dfs_v1";
 ////////////
+export * as processors from "./processors";
+export { checkForCycle } from "./processors";
---
+++ packages/zionbase/src/zionbase/Algo/Dfs/processors/checkForCycle/index.ts
@@ -0,0 +1,2 @@
+export { checkForCycle_v1 as checkForCycle } from "./checkForCycle_v1";
+export type { IcheckForCycle_v1 as IcheckForCycle } from "./checkForCycle_v1";
