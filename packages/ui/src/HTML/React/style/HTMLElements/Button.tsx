05d5d8a57acfd9ed409c62bcf74e914d46a68bc2
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-01-02 15:09:17 +0100

    add prop overload, primary and secondary styles
    test primary and secondary

--- packages/ui/src/HTML/React/style/HTMLElements/Button.tsx
+++ packages/ui/src/HTML/React/style/HTMLElements/Button.tsx
@@ -1,73 +1,125 @@
-import styled from "styled-components";
+import styled, {
+  css,
+  DefaultTheme,
+} from "styled-components";
 import { MouseEventHandler } from "react";
 import { NoizProps } from "../../lib/types";
 import { Position, Sizes } from "../../lib/global";
-import { checkCss } from "../../lib/util";
 
-export type ButtonStyle = NoizProps<
-  {
-    primary?: boolean;
-    secondary?: boolean;
-    disabled?: boolean;
-    onClick?: MouseEventHandler<HTMLButtonElement>;
-    size?: Sizes;
-    position?: Position;
-    zIndex?: string;
-    gridArea?: string;
-    child?: { justifySelf?: string };
-  },
-  true
->;
+const INTENT_ERROR =
+  "Button component cannot be primary and secondary at the samte time";
 
-export interface IButton extends ButtonStyle {}
+interface Primary {
+  primary?: true;
+  secondary?: false;
+}
+
+interface Secondary {
+  primary?: false;
+  secondary?: true;
+}
+
+interface Props {
+  disabled?: boolean;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+  size?: Sizes;
+  position?: Position;
+  zIndex?: string;
+  gridArea?: string;
+  child?: { justifySelf?: string };
+}
+
+export interface ButtonStylePrimary
+  extends NoizProps<Primary & Props, true> {}
+
+export interface ButtonStyleSecondary
+  extends NoizProps<Secondary & Props, true> {}
+
+export interface IButtonPrimary
+  extends ButtonStylePrimary {}
+
+export interface IButtonSecondary
+  extends ButtonStyleSecondary {}
+
+export type IButton = IButtonPrimary | IButtonSecondary;
+
+const primary = (
+  props: ButtonStylePrimary & { theme: DefaultTheme }
+) => {
+  let palette_ryb = props.theme.palette_ryb;
+  let blue_green = palette_ryb.blue_green;
+  let bg_color = blue_green.setBrightness(48).value;
+  let border_color = blue_green.setBrightness(45).value;
+  return css`
+    background-color: ${bg_color};
+    border: 0.1rem solid ${border_color};
+    color: ${props.theme.palette.white};
+  `;
+};
+
+const secondary = (
+  props: ButtonStyleSecondary & { theme: DefaultTheme }
+) => {
+  let palette_ryb = props.theme.palette_ryb;
+  let blue_green = palette_ryb.blue_green;
+  let bg_color = props.theme.primary.backgroundColor;
+  let color = blue_green.setBrightness(55).value;
+  let border_color = blue_green.setBrightness(55).value;
+  return css`
+    background-color: ${bg_color};
+    border: 0.1rem solid ${border_color};
+    color: ${color};
+  `;
+};
+
+const primary_hover = (
+  props: ButtonStylePrimary & { theme: DefaultTheme }
+) => {
+  let palette_ryb = props.theme.palette_ryb;
+  let blue_green = palette_ryb.blue_green;
+  let bg_color = blue_green.setBrightness(52).value;
+  let border_color = blue_green.setBrightness(65).value;
+  return css`
+    background-color: ${bg_color};
+    border: 0.1rem solid ${border_color};
+    color: ${props.theme.primary.color};
+  `;
+};
 
 export const Button = styled.button<IButton>`
-  line-height: 1.2;
-  font-size: 70%;
   place-self: center;
   border: 0;
-  border-radius: 10px;
-  text-align: center;
-  overflow: hidden;
-  padding: 0;
+  border-radius: 1rem;
+  padding: 0.23rem 1.5rem;
   margin: 0;
   cursor: pointer;
-  display: grid;
-  ${props => checkCss("gridArea", props)}
-  ${props => checkCss("zIndex", props)}
-  border: 1px solid ${props => {
-    return props.theme.primary.borderColor;
+
+  ${props => {
+    const isPrimary = props.primary;
+    const isSecondary = props.secondary;
+    if (isPrimary && isSecondary)
+      throw new Error(INTENT_ERROR);
+
+    if (isPrimary) {
+      return primary(props);
+    } else if (isSecondary) {
+      return secondary(props);
+    } else return null;
   }};
-  position: ${props =>
-    props.position ? "fixed" : "static"};
-  bottom: ${props => {
-    if (!props.position) return;
-    return props.position.bottom
-      ? props.position.bottom
-      : "10%";
-  }};
-  right: ${props => {
-    if (!props.position) return;
-    return props.position.right
-      ? props.position.right
-      : "7%";
-  }};
-  &:first-child {
-    display: grid;
-    justify-content: center;
-    justify-items: center;
-    justify-self: ${props => {
-      if (!props.child || !props.child.justifySelf)
-        return "center";
-      return props.child.justifySelf;
-    }};
-  }
+
   &:hover {
     cursor: pointer;
+    ${props => {
+      const isPrimary = props.primary;
+      if (isPrimary) {
+        return primary_hover(props);
+      } else return null;
+    }};
   }
-  text {
-    margin: 0;
-    place-self: center;
-    font-size: x-small;
+
+  &:active {
+    position: relative;
+    top: 1px;
+    left: 1px;
   }
 `;
--- packages/ui/src/pages/styles/html-elements/button/index.tsx
+++ packages/ui/src/pages/styles/html-elements/button/index.tsx
@@ -2,5 +2,16 @@ import React from "react";
 import { Button } from "../../../../HTML";
 
 export default function index() {
-  return <Button>Sono un Button</Button>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "1rem",
+      }}
+    >
+      <Button primary>Sono un Button primario</Button>
+      <Button secondary>Sono un Button secondario</Button>
+    </div>
+  );
 }
