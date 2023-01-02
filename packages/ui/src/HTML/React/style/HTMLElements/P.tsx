981f00ee7115aaeb2112928da09689b772043afc
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-01-02 16:01:01 +0100

    add
    add more context

--- packages/ui/src/HTML/React/style/HTMLElements/P.tsx
+++ packages/ui/src/HTML/React/style/HTMLElements/P.tsx
@@ -1,11 +1,78 @@
-import styled from "styled-components";
+import styled, {
+  css,
+  DefaultTheme,
+} from "styled-components";
 
-export interface IP {
-  bold?: boolean;
-  dimmed?: boolean;
+export interface light {
+  light?: true;
+  lighter?: false;
+  bold?: false;
+  bolder?: false;
 }
 
-export const P = styled.p<IP>`
-  ${props => (props.bold ? "font-weight: bold;" : "")}
-  ${props => (props.dimmed ? "color: grey;" : "")}
+export interface lighter {
+  lighter?: true;
+  light?: false;
+  bold?: false;
+  bolder?: false;
+}
+
+export interface bold {
+  bold?: true;
+  bolder?: false;
+  lighter?: false;
+  light?: false;
+}
+
+export interface bolder {
+  bolder?: true;
+  bold?: false;
+  lighter?: false;
+  light?: false;
+}
+
+export interface IPCommon {
+  dimmed?: boolean;
+  italic?: boolean;
+}
+
+type weights = light | lighter | bold | bolder;
+
+type IP = IPCommon & weights;
+
+const italic = css`
+  font-style: italic;
+`;
+
+const bold = css`
+  font-weight: bold;
+`;
+
+const bolder = css`
+  font-weight: bolder;
+`;
+
+const light = css`
+  font-weight: 300;
+`;
+
+const lighter = css`
+  font-weight: 300;
+`;
+
+const dimmed = (props: IP & { theme: DefaultTheme }) => {
+  if (!props.dimmed)
+    throw new Error("prop dimmed not present");
+  return css`
+    color: ${props.theme.palette.grey};
+  `;
+};
+
+export const P = styled.p<IP & { theme: DefaultTheme }>`
+  ${props => (props.light ? light : null)}
+  ${props => (props.lighter ? lighter : null)}
+  ${props => (props.bold ? bold : null)}
+  ${props => (props.bolder ? bolder : null)}
+  ${props => (props.dimmed ? dimmed(props) : null)}
+  ${props => (props.italic ? italic : null)}
 `;
--- packages/ui/src/pages/styles/html-elements/p/index.tsx
+++ packages/ui/src/pages/styles/html-elements/p/index.tsx
@@ -1,6 +1,56 @@
 import React from "react";
+import styled from "styled-components";
 import { P } from "../../../../HTML";
 
+const Container = styled.div`
+  display: inline;
+  gap: 0.5rem;
+  padding: 2rem;
+  p {
+    margin-left: 0.2rem;
+    display: inline;
+  }
+`;
+
 export default function index() {
-  return <P>Sono un P</P>;
+  return (
+    <Container>
+      <P>
+        Sono un P. Posso essere messo vicino ad altri P.
+      </P>
+      <P dimmed>Io sono un P, messo</P>
+      <P dimmed bold>
+        di fianco
+      </P>
+      <P dimmed>
+        al P precedente, e la mia props `dimmed` è valutata
+        "true".
+      </P>
+      <P>Per funzionare correttamente</P>
+      <P bold>uno di fianco all'altro</P>
+      <P>
+        dobbiamo essere inseriti (noi P), all'interno di un
+        container con `display: inline`".
+      </P>
+      <P>Posso anche essere usato per rendere un testo</P>
+      <P italic>italic</P>
+      <P>o</P>
+      <P italic bold>
+        italic-bold.
+      </P>
+      <br />
+      <P light>Posso scrivere in maniera più leggera,</P>
+      <P light>fine a diventare</P>
+      <P lighter>super fine. Io sono superfine,</P>
+      <P light>
+        mentre io sono fine, non è che la differenza si
+        noti molto.
+      </P>
+      <br />
+      <P>ma posso anche essere</P>
+      <P bold>bold</P>
+      <P>oppure</P>
+      <P bolder>bolder</P>
+    </Container>
+  );
 }
