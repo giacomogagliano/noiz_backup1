7a951e16b3e7d257e573bba68f2a42b184e8096b
ariannatnl <arianna.minucci@me.com>
2023-01-09 11:15:48 +0100

    padding common

--- packages/ui/src/HTML/React/style/common/index.ts
+++ packages/ui/src/HTML/React/style/common/index.ts
@@ -2,6 +2,7 @@ export * from "./border";
 export * from "./fillable";
 export * as position from "./position";
 export * from "./position";
+export * from "./areaBorderPadding";
 export {};
 
 //// Common
