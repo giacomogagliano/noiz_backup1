23d30d53c8b4a428686880e7bee3aaa468fef930
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-02-06 22:53:29 +0100

    test next module

--- packages/ui/test/Html/Next/staticProps.test.ts
+++ packages/ui/test/Html/Next/staticProps.test.ts
@@ -15,3 +15,74 @@ describe("if staticProps function is correctly exported", () => {
     expect(res).to.be.true;
   });
 });
+
+describe("class functionalities", () => {
+  // with RAM
+  const data = { id: "", name: "bub" };
+  type datatype = typeof data;
+  const database = [data, { id: "1", name: "ciii" }];
+  const origin = { projects: [data] };
+  type props = typeof origin;
+  const o: props = { projects: [data] };
+  o;
+  const { getStaticProps } = getStatic<datatype>(
+    "RAM",
+    origin,
+    "projects",
+    database
+  );
+  getStaticProps;
+  // with fetch
+  const { getStaticProps: gr } = getStatic<{}>(
+    "fetch",
+    "http://localhost:3000/api/nfts",
+    "data"
+  );
+  gr;
+});
+
+describe("getStaticPath and getStaticProps", () => {
+  const data = { id: "", name: "bub" };
+  type datatype = typeof data;
+  const database = [data, { id: "1", name: "ciii" }];
+  const origin = { project: data };
+  type props = typeof origin;
+  const o: props = { project: { id: "", name: "" } };
+  o;
+  const query = { id: "" };
+  const { getStaticPaths, getStaticProps } = getStatic<
+    datatype,
+    { id: string }
+  >("RAM", origin, "project", database, query, "id", [
+    { params: { id: "0" } },
+    { params: { id: "1" } },
+    { params: { id: "2" } },
+  ]);
+  it("should contain getStaticPaths function", () => {
+    const regex = /getStaticPaths/g;
+    expect(regex.test(getStaticPaths.name)).to.be.true;
+  });
+  it("should contain getStaticPaths", () => {
+    const regex = /getStaticProps/g;
+    expect(regex.test(getStaticProps.name)).to.be.true;
+  });
+
+  // with fetch
+  const { getStaticPaths: gspt, getStaticProps: gspr } =
+    getStatic<{}, { id: string }>(
+      "fetch",
+      "http://localhost:3000/api/nfts",
+      "data",
+      undefined,
+      { id: "" },
+      "id"
+    );
+  it("should contain getStatic", () => {
+    const regex = /getStaticPaths/g;
+    expect(regex.test(gspt.name)).to.be.true;
+  });
+  it("should contain getStaticPaths", () => {
+    const regex = /getStaticProps/g;
+    expect(regex.test(gspr.name)).to.be.true;
+  });
+});
