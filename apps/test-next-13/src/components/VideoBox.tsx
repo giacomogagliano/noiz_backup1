3a0836281347b9db1e1627739345c14be112f733
ariannatnl <arianna.minucci@me.com>
2023-02-13 17:05:53 +0100

    make component videoBox

--- apps/test-next-13/src/components/VideoBox.tsx
+++ apps/test-next-13/src/components/VideoBox.tsx
@@ -1,50 +1,71 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+const IconContainer = css`
+  width: 70px;
+  height: auto;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 1;
+`;
+
+const AutoplayVideo = css`
+  display: block;
+  margin: auto;
+  width: 100%;
+  max-width: 800px;
+  height: auto;
+  border-radius: 10px;
+  filter: brightness(0.9);
+  box-shadow: 0 0 30px rgba(0, 0, 0, 0.2);
+`;
+
+const AutoplayVideoContainer = css`
+  position: relative;
+  display: block;
+  margin-top: 20px;
+  margin-bottom: 50px;
+`;
 
 const VideoBoxComponent = styled.div`
-  .autoplay-video-container {
-    position: relative;
-    display: block;
-    margin-top: 20px;
-    margin-bottom: 50px;
+  display: grid;
+  place-items: center;
 
-    .youtube-play-icon {
-      width: 70px;
-      height: auto;
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-      z-index: 1;
-    }
-    .autoplay-video {
-      display: block;
-      margin: auto;
-      width: 100%;
-      max-width: 800px;
-      height: auto;
-      border-radius: 10px;
-      filter: brightness(0.9);
-      box-shadow: 0 0 30px rgba(0, 0, 0, 0.2);
-    }
+  video {
+    ${AutoplayVideo}
+  }
+  img {
+    ${IconContainer}
+  }
+  a {
+    ${AutoplayVideoContainer}
   }
 `;
 
-export const VideoBox = () => (
-  <VideoBoxComponent>
-    <a
-      className="autoplay-video-container"
-      href="https://www.youtube.com/watch?v=Uu1TuE6RdKM"
-      target="_blank"
-    >
-      <img
-        className="youtube-play-icon"
-        src="https://i.imgur.com/trXdlO5.png"
-      ></img>
-      <video className="autoplay-video" autoPlay loop muted>
-        /////// autoplay="" loop="" muted="" //////////
-        <source src="https://i.imgur.com/JFknZrb.mp4" type="video/mp4" />
-      </video>
-    </a>
-  </VideoBoxComponent>
-);
+interface VideoBoxElement {
+  src: string;
+}
+export const VideoBox = ({
+  href,
+  icon,
+  video,
+}: {
+  href: string;
+  icon: VideoBoxElement;
+  video: VideoBoxElement;
+}) => {
+  const target = "_blank";
+  const type = "video/mp4";
+  return (
+    <VideoBoxComponent>
+      <a href={href} target={target}>
+        <img src={icon.src}></img>
+        <video autoPlay loop muted>
+          <source src={video.src} type={type} />
+        </video>
+      </a>
+    </VideoBoxComponent>
+  );
+};
