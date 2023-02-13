bf83578f31dfc413777085c6a884988af1740c1c
ariannatnl <arianna.minucci@me.com>
2023-02-13 18:50:30 +0100

    exported card

--- apps/test-next-13/src/components/Card.tsx
+++ apps/test-next-13/src/components/Card.tsx
@@ -1,53 +1,143 @@
 import React from "react";
 import styled from "styled-components";
 
-const CardComponent = styled.div``;
+const StyledCard = styled.div`
+  display: flex;
+  overflow: visible;
+  padding-top: 50px;
+  padding-right: 50px;
+  padding-left: 50px;
+  flex-direction: column;
+  border-style: solid;
+  border-width: 1px;
+  border-color: #e9ebee;
+  border-radius: 40px;
+  background-color: #fff;
+  box-shadow: 1px 1px 60px 0 rgb(0 0 0 / 15%);
+  @media screen and (max-width: 479px) {
+    margin-bottom: 0px;
+    padding: 40px 30px;
+    border-radius: 30px;
+  }
+  @media screen and (max-width: 991px) {
+    max-width: 600px;
+  }
+  #feature-header {
+    display: flex;
+    margin-bottom: 0px;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: flex-start;
+    @media screen and (max-width: 991px) {
+      top: -80px;
+    }
+  }
+  #feature-heading {
+    color: #0082c9;
+    display: block;
+    margin-top: 0px;
+    margin-right: 20px;
+    margin-bottom: 40px;
+    font-size: 24px;
+    line-height: 29px;
+    font-weight: 600;
+    @media screen and (max-width: 479px) {
+      margin-right: 30px;
+      margin-bottom: 30px;
+      font-size: 20px;
+      line-height: 26px;
+    }
+  }
+  #feature-icon {
+    width: 100%;
+    max-width: 120px;
+    border-radius: 22%;
+    box-shadow: 3px 3px 20px 0 rgb(0 0 0 / 20%);
+    transform: translate(0px, -70%);
+    box-shadow: 3px 3px 20px 0 rgb(46 120 230 / 47%);
+    @media screen and (max-width: 479px) {
+      max-width: 80px;
+      margin-right: -10px;
+      margin-left: -20px;
+      transform: translate(0px, -80%);
+    }
+  }
+  #feature-content {
+    display: flex;
+    overflow: hidden;
+    margin-right: -51px;
+    margin-bottom: -1px;
+    margin-left: -50px;
+    padding-left: 50px;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: flex-end;
+    flex: 1;
+    border-bottom-left-radius: 40px;
+    border-bottom-right-radius: 40px;
+    @media screen and (max-width: 479px) {
+      margin-right: -31px;
+      margin-bottom: -43px;
+      margin-left: -30px;
+      padding-left: 30px;
+      border-bottom-left-radius: 30px;
+      border-bottom-right-radius: 30px;
+    }
+  }
+  .column-12 {
+    @media screen and (max-width: 479px) {
+      padding-right: 0px;
+      padding-left: 0px;
+    }
+  }
+  .w-col {
+    position: relative;
+    float: left;
+    width: 100%;
+    min-height: 1px;
+    padding-left: 10px;
+    padding-right: 10px;
+    @media screen and (max-width: 767px) {
+      width: 100%;
+      left: auto;
+      right: auto;
+    }
+    @media screen and (max-width: 479px) {
+      width: 100%;
+    }
+  }
+  .w-col-10 {
+    width: 83.33333333%;
+  }
+
+  .w-col-2 {
+    width: 16.66666667%;
+  }
+`;
 
 export const Card = () => (
-  <CardComponent>
-    {" "}
-    <div className="feature-card">
-      <div className="feature-header">
-        <h3 className="feature-heading feature-heading-nextcloud">
-          Run your private cloud with Nextcloud.
-        </h3>
-        <img
-          src="./assets/icon_4.svg"
-          loading="lazy"
-          className="feature-icon feature-icon-nextcloud"
-        ></img>
-      </div>
-      <div className="feature-content">
-        <div className="columns-3 w-row">
-          <div className="column-12 w-col w-col-10">
-            <p className="paragraph text-left">
-              An entirely self-hosted Google Drive replacement — store your
-              documents, calendar, contacts, photos and videos on your Umbrel
-              with Nextcloud instead of Google’s servers.
-            </p>
-          </div>
-          <div className="w-col w-col-2"></div>
+  <StyledCard>
+    <div id="feature-header">
+      <h3 id="feature-heading">Run your private cloud with Nextcloud.</h3>
+      <img src="./assets/icon_4.svg" loading="lazy" id="feature-icon"></img>
+    </div>
+    <div id="feature-content">
+      <div>
+        <div className="column-12 w-col w-col-10">
+          <p className="paragraph text-left">
+            An entirely self-hosted Google Drive replacement — store your
+            documents, calendar, contacts, photos and videos on your Umbrel with
+            Nextcloud instead of Google’s servers.
+          </p>
         </div>
-        <img
-          src="./assets/Group-1601.jpg"
-          loading="eager"
-          className="feature-image"
-        ></img>
+        <div className="w-col w-col-2"></div>
       </div>
-      <div></div>
+      <img
+        src="./assets/Group-1601.jpg"
+        loading="eager"
+        className="feature-image"
+      ></img>
     </div>
-    <div className="feature-card feature-card-photoprism">
-      <div className="feature-header">
-        <h3 className="feature-heading feature-heading-photoprism">
-          Store your photos and videos with PhotoPrism.
-        </h3>
-        <img
-          className="feature-icon feature-icon-photoprism"
-          src=""
-          loading="lazy"
-        ></img>
-      </div>
-      <div className="feature-content"></div>
-    </div>
-  </CardComponent>
+    <div></div>
+  </StyledCard>
 );
--- apps/test-next-13/src/components/FullPage.tsx
+++ apps/test-next-13/src/components/FullPage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { Card } from "./Card";
 import { VideoBox } from "./VideoBox";
 
 const FullPageComponent = styled.div`
@@ -96,26 +97,6 @@ const FullPageComponent = styled.div`
               }
             }
           }
-          .w-row {
-            margin-left: -10px;
-            margin-right: -10px;
-            @media screen and (max-width: 767px) {
-              margin-left: 0;
-              margin-right: 0;
-            }
-            &:after {
-              clear: both;
-            }
-            &:before,
-            &:after {
-              content: " ";
-              display: table;
-              grid-column-start: 1;
-              grid-row-start: 1;
-              grid-column-end: 2;
-              grid-row-end: 2;
-            }
-          }
         }
       }
     }
@@ -163,49 +144,7 @@ export const FullPage = () => {
               One that's powered by you. <span className="text-span-2"></span>
             </h3>
             <div className="w-layout-grid grid-5">
-              <div className="feature-card">
-                <div className="feature-header">
-                  <h3 className="feature-heading feature-heading-nextcloud">
-                    Run your private cloud with Nextcloud.
-                  </h3>
-                  <img
-                    src="./assets/icon_4.svg"
-                    loading="lazy"
-                    className="feature-icon feature-icon-nextcloud"
-                  ></img>
-                </div>
-                <div className="feature-content">
-                  <div className="columns-3 w-row">
-                    <div className="column-12 w-col w-col-10">
-                      <p className="paragraph text-left">
-                        An entirely self-hosted Google Drive replacement — store
-                        your documents, calendar, contacts, photos and videos on
-                        your Umbrel with Nextcloud instead of Google’s servers.
-                      </p>
-                    </div>
-                    <div className="w-col w-col-2"></div>
-                  </div>
-                  <img
-                    src="./assets/Group-1601.jpg"
-                    loading="eager"
-                    className="feature-image"
-                  ></img>
-                </div>
-                <div></div>
-              </div>
-              <div className="feature-card feature-card-photoprism">
-                <div className="feature-header">
-                  <h3 className="feature-heading feature-heading-photoprism">
-                    Store your photos and videos with PhotoPrism.
-                  </h3>
-                  <img
-                    className="feature-icon feature-icon-photoprism"
-                    src=""
-                    loading="lazy"
-                  ></img>
-                </div>
-                <div className="feature-content"></div>
-              </div>
+              <Card></Card>
             </div>
           </div>
         </div>
