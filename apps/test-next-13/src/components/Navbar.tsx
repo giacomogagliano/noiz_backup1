0cbddb079b2fd1d6995c8a75b46dffc774ee27c9
ariannatnl <arianna.minucci@me.com>
2023-02-13 17:06:30 +0100

    make styled CSS

--- apps/test-next-13/src/components/Navbar.tsx
+++ apps/test-next-13/src/components/Navbar.tsx
@@ -1,119 +1,154 @@
 import React, { HTMLAttributeAnchorTarget } from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+const DivBlock18 = css`
+  display: flex;
+  width: 100%;
+  max-width: 990px;
+  justify-content: space-between;
+`;
+
+const GitHubStars = css`
+  width: 84px;
+  height: auto;
+`;
+
+const DivBlock19 = css`
+  display: flex;
+  align-items: center;
+`;
+
+const HeaderLogo = css`
+  height: 30px;
+  margin-top: 18px;
+  margin-bottom: 18px;
+`;
+
+const HeaderStickyLink = css`
+  color: #1d1d1f;
+`;
+
+const DXsNone = css`
+  @media screen and (max-width: 479px) {
+    display: none;
+  }
+`;
+
+const HeaderStickyLinkP = css`
+  background-color: #5351fb;
+  color: #fff;
+`;
+
+const HeaderLinkPrimary = css`
+  padding: 7px 20px;
+  border-radius: 16px;
+  background-color: #fff;
+  color: #1d1d1f;
+  @media screen and (max-width: 479px) {
+    padding: 5px 16px;
+  }
+`;
+
+const HeaderLink = css`
+  position: relative;
+  margin-right: 9px;
+  margin-left: 9px;
+  text-decoration: none;
+  margin-top: 0px;
+  color: #fff;
+  font-size: 17px;
+  line-height: 20px;
+  font-weight: 400;
+  @media screen and (max-width: 479px) {
+    font-size: 14px;
+  }
+  @media screen and (max-width: 479px) {
+    margin-right: 0px;
+    margin-left: 14px;
+  }
+  @media screen and (max-width: 767px) {
+    font-size: 14px;
+  }
+`;
+
+const HtmlHeader2 = css`
+  a {
+    cursor: pointer;
+  }
+  @media screen and (max-width: 640px) {
+    display: none;
+  }
+`;
+
+const LinkNav = css`
+  ${HeaderLink}
+  ${HeaderStickyLink}
+`;
 
 const NavbarComponent = styled.div`
-  .div-block-18 {
-    display: flex;
-    width: 100%;
-    max-width: 990px;
-    justify-content: space-between;
+  #div-block-18 {
+    ${DivBlock18}
   }
-  .div-block-19 {
-    display: flex;
-    align-items: center;
+  #div-block-19 {
+    ${DivBlock19}
   }
   a {
     background-color: transparent;
     text-decoration: underline;
   }
-  .header-logo {
-    height: 30px;
-    margin-top: 18px;
-    margin-bottom: 18px;
+  #header-logo {
+    ${HeaderLogo}
   }
-  .header-link {
-    position: relative;
-    margin-right: 9px;
-    margin-left: 9px;
-    text-decoration: none;
-    margin-top: 0px;
-    color: #fff;
-    font-size: 17px;
-    line-height: 20px;
-    font-weight: 400;
-    @media screen and (max-width: 479px) {
-      font-size: 14px;
-    }
-    @media screen and (max-width: 479px) {
-      margin-right: 0px;
-      margin-left: 14px;
-    }
-
-    @media screen and (max-width: 767px) {
-      font-size: 14px;
-    }
-    &.d-xs-none {
-      @media screen and (max-width: 479px) {
-        display: none;
-      }
-    }
-    &.header-link-primary {
-      padding: 7px 20px;
-      border-radius: 16px;
-      background-color: #fff;
-      color: #1d1d1f;
-      @media screen and (max-width: 479px) {
-        padding: 5px 16px;
-      }
-      &.header-sticky-link {
-        background-color: #5351fb;
-        color: #fff;
-      }
-    }
-    &.header-sticky-link {
-      color: #1d1d1f;
-    }
+  #github-link-nav {
+    ${HtmlHeader2}
   }
-  .html-embed-2 {
-    a {
-      cursor: pointer;
-    }
-    @media screen and (max-width: 640px) {
-      display: none;
-    }
-    .github-stars {
-      width: 84px;
-      height: auto;
-    }
+  #link-nav {
+    ${LinkNav}
+  }
+  #link-nav-hidden {
+    ${LinkNav}
+    ${DXsNone}
+  }
+  #install-now {
+    ${HeaderLink}
+    ${HeaderLinkPrimary}
+    ${HeaderStickyLinkP}
+  }
+  #github-stars {
+    ${GitHubStars}
   }
 `;
 
 export const Navbar = () => (
   <NavbarComponent>
-    <div className="div-block-18">
+    <div id="div-block-18">
       <img
         src="assets/gotek-write.svg"
         loading="lazy"
         alt=""
-        className="header-logo"
+        id="header-logo"
       />
-      <div className="div-block-19">
-        <a
-          href="#app-store"
-          className="header-link header-sticky-link d-xs-none "
-        >
+      <div id="div-block-19">
+        <a href="" id="link-nav-hidden">
           <div>Apple Store</div>
         </a>
         <a
           href="https://community.getumbrel.com/"
           target="_blank"
-          className="header-link header-sticky-link d-xs-none "
+          id="link-nav-hidden"
         >
           <div>Community</div>
         </a>
-        <a href="" target="_blank" className="header-link header-sticky-link ">
+        <a href="" target="_blank" id="link-nav">
           <div>We are Hiring!</div>
         </a>
-        <a
-          href="#start"
-          className="header-link header-link-primary header-sticky-link "
-        >
+        <a href="#start" id="install-now">
           <div>Install Now</div>
         </a>
-        <div className="html-embed-2 w-embed">
+        <div id="github-link-nav">
           <a href="https://github.com/getumbrel/umbrel" target="_blank">
             <img
-              className="github-stars"
+              id="github-stars"
               alt="Star Umbrel on GitHub"
               src="https://img.shields.io/github/stars/getumbrel/umbrel?label=Star&style=social"
             ></img>
