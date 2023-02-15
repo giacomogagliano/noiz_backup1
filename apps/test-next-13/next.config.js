b3c703711bf72a92e76fba8d3058b3d5a66e80d9
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-02-15 11:27:46 +0100

    edit

--- apps/test-next-13/next.config.js
+++ apps/test-next-13/next.config.js
@@ -1,17 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// import { config } from "dotenv";
-// config();
-
-// export default {
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-//   reactStrictMode: true,
-//   compiler: {
-//     styledComponents: true,
-//   },
-//   // env: { infura: process.env.INFURA },
-// };
 module.exports = {
   webpack: config => {
     config.resolve.fallback = {
