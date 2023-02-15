dbcf46ed550e41f0ecbbff1e831265ed4d39098b
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-02-15 21:52:36 +0100

    make server side

--- apps/test-next-13/src/components/StylesSheet.tsx
+++ apps/test-next-13/src/components/StylesSheet.tsx
@@ -405,3 +405,46 @@ export const StyledCard = styled.div`
     ${ParagraphTextLeft}
   }
 `;
+
+const IconContainer = css`
+  width: 70px;
+  height: auto;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 1;
+`;
+
+const AutoplayVideo = css`
+  display: block;
+  margin: auto;
+  width: 100%;
+  max-width: 800px;
+  height: auto;
+  border-radius: 10px;
+  filter: brightness(0.9);
+  box-shadow: 0 0 30px rgba(0, 0, 0, 0.2);
+`;
+
+const AutoplayVideoContainer = css`
+  position: relative;
+  display: block;
+  margin-top: 20px;
+  margin-bottom: 50px;
+`;
+
+export const VideoBoxComponent = styled.div`
+  display: grid;
+  place-items: center;
+
+  video {
+    ${AutoplayVideo}
+  }
+  img {
+    ${IconContainer}
+  }
+  a {
+    ${AutoplayVideoContainer}
+  }
+`;
