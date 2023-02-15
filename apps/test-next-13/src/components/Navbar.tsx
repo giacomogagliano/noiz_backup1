4cc8694d5e58d390a99a2513b8b79785ace394bb
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-02-16 00:24:48 +0100

    edit

--- apps/test-next-13/src/components/Navbar.tsx
+++ apps/test-next-13/src/components/Navbar.tsx
@@ -1,46 +1,103 @@
 import React from "react";
+import { GitHubLinks } from "./GitHubLink";
 import { CssProps, NavbarComponent } from "./StylesSheet";
+interface HreftargetProps {
+  href: string;
+  target?: string;
+  id: string;
+  text: string;
+}
+class HreftargetProps {
+  constructor(props: HreftargetProps) {
+    this.href = props.href;
+    this.id = props.id;
+    this.text = props.text;
+    if (props.target) this.target = props.target;
+  }
+}
 
-export const Navbar = ({ bgcolor, buttonBgColor, color }: CssProps) => (
-  <NavbarComponent
-    bgcolor={bgcolor}
-    buttonBgColor={buttonBgColor}
-    color={color}
-  >
-    <div id="div-block-18">
-      <img
-        src="assets/gotek-write.svg"
-        loading="lazy"
-        alt=""
-        id="header-logo"
-      />
-      <div id="div-block-19">
-        <a href="" id="link-nav-hidden">
-          <div>Apple Store</div>
-        </a>
-        <a
-          href="https://community.getumbrel.com/"
-          target="_blank"
-          id="link-nav-hidden"
-        >
-          <div>Community</div>
-        </a>
-        <a href="" target="_blank" id="link-nav">
-          <div>We are Hiring!</div>
-        </a>
-        <a href="#start" id="install-now">
-          <div>Install Now</div>
-        </a>
-        <div id="github-link-nav">
-          <a href="https://github.com/getumbrel/umbrel" target="_blank">
-            <img
-              id="github-stars"
-              alt="Star Umbrel on GitHub"
-              src="https://img.shields.io/github/stars/getumbrel/umbrel?label=Star&style=social"
-            ></img>
-          </a>
+const HreTargetA = ({ href, target, id, text }: HreftargetProps) => (
+  <a href={href} target={target} id={id}>
+    <div>{text}</div>
+  </a>
+);
+
+const Links = () => {
+  // ids
+  const LINK_NAV_HID = "link-nav-hidden";
+  const LINK_NAV = "link-nav";
+  const GITHUB_LINK_NAV = "github-link-nav";
+  const ACTION_BTN = "action-btn";
+  // hrefs
+  const LINK_1 = "";
+  const LINK_COMM = "https://community.getumbrel.com/";
+  const START_LINK = "#start";
+  // target
+  const BLNK = "_blank";
+  // github
+  const ORG = "Zion-PTC";
+  const REPO = "noiz-network-state";
+
+  const APPLE_STORE = new HreftargetProps({
+    id: LINK_NAV_HID,
+    href: LINK_1,
+    text: "Apple Store",
+  });
+
+  const COMMUNITY = new HreftargetProps({
+    id: LINK_NAV_HID,
+    href: LINK_COMM,
+    text: "Community",
+  });
+
+  const WEHIRING = new HreftargetProps({
+    id: LINK_NAV,
+    href: LINK_1,
+    text: "We are hiring!!",
+  });
+
+  const INSTALL_NOW = new HreftargetProps({
+    id: ACTION_BTN,
+    href: START_LINK,
+    text: "Install Now",
+  });
+
+  return (
+    <>
+      <HreTargetA {...APPLE_STORE}></HreTargetA>
+      <HreTargetA {...COMMUNITY}></HreTargetA>
+      <HreTargetA {...WEHIRING}></HreTargetA>
+      <HreTargetA {...INSTALL_NOW}></HreTargetA>
+      <GitHubLinks id={GITHUB_LINK_NAV} org={ORG} repo={REPO}></GitHubLinks>
+    </>
+  );
+};
+
+type NavBarProps = { src?: string };
+export const Navbar = ({
+  bgcolor,
+  buttonBgColor,
+  color,
+  src,
+}: CssProps & NavBarProps) => {
+  const SRC = "assets/gotek-write.svg";
+  const LOADING = "lazy";
+  const ALT = "";
+  const IMG_ID = "header-logo";
+  const OUTER_CONTAINER_ID = "div-block-18";
+  const LINKS_CONTAINER_ID = "div-block-19";
+  return (
+    <NavbarComponent
+      bgcolor={bgcolor}
+      buttonBgColor={buttonBgColor}
+      color={color}
+    >
+      <div id={OUTER_CONTAINER_ID}>
+        <img src={SRC} loading={LOADING} alt={ALT} id={IMG_ID} />
+        <div id={LINKS_CONTAINER_ID}>
+          <Links></Links>
         </div>
       </div>
-    </div>
-  </NavbarComponent>
-);
+    </NavbarComponent>
+  );
+};
