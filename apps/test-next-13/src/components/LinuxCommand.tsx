3668b982028653ecab41ef205c7baf99684dbb90
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-02-15 21:57:37 +0100

    make ssr

--- apps/test-next-13/src/components/LinuxCommand.tsx
+++ apps/test-next-13/src/components/LinuxCommand.tsx
@@ -1,34 +1,5 @@
-"use client";
 import React from "react";
-import styled, { css } from "styled-components";
-
-const LinuxCommandContainer = css`
-  padding: 16px 20px;
-  border-radius: 8px;
-  background-color: hsla(0, 0%, 100%, 0.2);
-  -webkit-backdrop-filter: blur(20px);
-  backdrop-filter: blur(20px);
-  margin-bottom: 20px;
-`;
-const LinuxCommandCss = css`
-  font-family: Inconsolata, monospace;
-  color: #fff;
-  font-size: 20px;
-  line-height: 24px;
-  font-weight: 400;
-  @media screen and (max-width: 479px) {
-    font-size: 16px;
-  }
-`;
-
-const LinuxCommandComponent = styled.div`
-  #linux-command-container {
-    ${LinuxCommandContainer}
-  }
-  #linux-command {
-    ${LinuxCommandCss}
-  }
-`;
+import { LinuxCommandComponent } from "./StylesSheet";
 
 export const LinuxCommand = () => (
   <LinuxCommandComponent>
--- apps/test-next-13/src/components/StylesSheet.tsx
+++ apps/test-next-13/src/components/StylesSheet.tsx
@@ -591,3 +591,31 @@ export const HeaderComponent = styled.div<HeaderComponentType>`
 //     <span id="text-span"></span>
 //   </h1>
 // );
+
+const LinuxCommandContainer = css`
+  padding: 16px 20px;
+  border-radius: 8px;
+  background-color: hsla(0, 0%, 100%, 0.2);
+  -webkit-backdrop-filter: blur(20px);
+  backdrop-filter: blur(20px);
+  margin-bottom: 20px;
+`;
+const LinuxCommandCss = css`
+  font-family: Inconsolata, monospace;
+  color: #fff;
+  font-size: 20px;
+  line-height: 24px;
+  font-weight: 400;
+  @media screen and (max-width: 479px) {
+    font-size: 16px;
+  }
+`;
+
+export const LinuxCommandComponent = styled.div`
+  #linux-command-container {
+    ${LinuxCommandContainer}
+  }
+  #linux-command {
+    ${LinuxCommandCss}
+  }
+`;
