815bc6a317e080dc2ecc219b4b0b9b3da9abd6ad
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-02-15 21:01:21 +0100

    test again

--- apps/test-next-13/src/app/tests/loader/page.tsx
+++ apps/test-next-13/src/app/tests/loader/page.tsx
@@ -5,7 +5,7 @@ import styled from "styled-components";
 import { Loader } from "../../../classes/Loader";
 
 export const Style = styled.div`
-  position: fixed;
+  /* position: fixed; */
   color: green;
   .trigger-class {
     transition: background-color 1s;
@@ -18,29 +18,27 @@ export const Style = styled.div`
 export default function page() {
   const Element = ({ id }) => (
     <Style>
-      <div id={id} className="trigger-class">
+      <div key={0} id={id} className="trigger-class">
         sono l'elemento
       </div>
     </Style>
   );
   const cb = (entry: IntersectionObserverEntry) => {
-    // console.log(entry.target);
-
     entry.target.classList.toggle("show", entry.isIntersecting);
     // entry.target.setAttribute("style", "background-color: red;");
   };
-  const JSXElement = Element({ id: "trigger" });
+  const JSXElement = <Element key={0} id="trigger"></Element>;
   return (
     <>
+      <div
+        style={{ width: "100%", height: "1000px", backgroundColor: "orange" }}
+      ></div>
       <Loader
         triggerkey={".trigger-class"}
         elements={[JSXElement]}
         cb={cb}
-        rootMargin={"-200px"}
+        threshold={1}
       ></Loader>
-      <div
-        style={{ width: "100%", height: "1000px", backgroundColor: "orange" }}
-      ></div>
       <div
         id="merda"
         style={{ width: "100%", height: "1000px", backgroundColor: "cyan" }}
