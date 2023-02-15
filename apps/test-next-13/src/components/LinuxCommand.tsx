38e940afc4bf133d579bb92291b112aa62aafecd
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-02-15 21:02:11 +0100

    edit

--- apps/test-next-13/src/components/LinuxCommand.tsx
+++ apps/test-next-13/src/components/LinuxCommand.tsx
@@ -5,9 +5,9 @@ const LinuxCommandContainer = css`
   padding: 16px 20px;
   border-radius: 8px;
   background-color: hsla(0, 0%, 100%, 0.2);
-
   -webkit-backdrop-filter: blur(20px);
   backdrop-filter: blur(20px);
+  margin-bottom: 20px;
 `;
 const LinuxCommandCss = css`
   font-family: Inconsolata, monospace;
