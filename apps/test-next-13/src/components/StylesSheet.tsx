7e0606a68653509409dac1fba091851ce4b8327e
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-02-15 21:02:40 +0100

    delete, non so cosa facciano effettivamente

--- apps/test-next-13/src/components/StylesSheet.tsx
+++ apps/test-next-13/src/components/StylesSheet.tsx
@@ -97,10 +97,10 @@ export const Body = styled.body`
     margin-top: 20px;
   }
   p {
-    transform: translate3d(0px, 14px, 0px) scale3d(1, 1, 1) rotateX(0deg)
+    /* transform: translate3d(0px, 14px, 0px) scale3d(1, 1, 1) rotateX(0deg)
       rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg);
     opacity: 1;
-    transform-style: preserve-3d;
+    transform-style: preserve-3d; */
   }
 
   img {
@@ -109,85 +109,6 @@ export const Body = styled.body`
     display: inline-block;
     border: 0;
   }
-
-  //////////
-  //////////
-  //////////
-  //////////
-  //////////
-  //////////
-  //////////
-  //////////
-  //////////
-  //////////
-  //////////
-  //////////
-  //////////
-  /* .section.hero {
-  padding-top: 0px;
-  padding-bottom: 100px;
-  background-image: url(assets/hero-bg_1.jpg);
-  background-image: url(assets/hero-bg_1.jpg),
-    linear-gradient(180deg, #e1583b, #4d2d47 40%, #20122d 74%);
-  background-position: 50% 0%, 0px 0px;
-} */
-
-  /* .text-span {
-  background-image: linear-gradient(86deg, #f86339, #fdc945);
-  -webkit-background-clip: text;
-  background-clip: text;
-  -webkit-text-fill-color: transparent;
-  .text-span-2 {
-    box-decoration-break: clone;
-  }
-} */
-
-  /* .paragraph {
-  color: black;
-  text-align: center;
-  @media screen and (max-width: 479px) {
-    font-size: 20px;
-    line-height: 26px;
-  }
-  .text-left {
-    text-align: left;
-    position: relative;
-    z-index: 1;
-    margin-bottom: 10px;
-    text-align: left;
-    .text-white {
-      color: #fff;
-    }
-  }
-} */
-
-  /* .text-center {
-  text-align: center;
-  .text-white.text-hero {
-    margin-bottom: 20px;
-    font-weight: 400;
-    opacity: 0.5;
-    .text-muted {
-      opacity: 0.5;
-    }
-    &.text-small {
-      font-size: 18px;
-      line-height: 22px;
-    }
-  }
-} */
-
-  /* .sub-heading.text-center.mb-80 {
-  margin-bottom: 80px;
-  font-family: Inter, sans-serif;
-  font-weight: 600;
-}
-.image-18 {
-  display: block;
-  margin-top: 60px;
-  margin-right: auto;
-  margin-left: auto;
-} */
 `;
 
 export const FromTop = styled.div<{ top?: string; transition?: string }>`
