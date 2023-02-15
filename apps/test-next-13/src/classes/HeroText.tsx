bcb14fb6e522875b2663faa7f6c60ffa1605b723
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-02-15 16:31:40 +0100

    create

---
+++ apps/test-next-13/src/classes/HeroText.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import React, { Component } from "react";
+import styled, { css } from "styled-components";
+import { Loader } from "../components/Loader";
+
+const Heading = css`
+  margin-bottom: 40px;
+  font-size: 90px;
+  line-height: 96px;
+  font-weight: 600;
+  text-align: center;
+  color: white;
+`;
+const HeadingHero = css<{ top: string; opacity: string }>`
+  position: relative;
+  top: ${props => {
+    return props.top;
+  }};
+  opacity: ${props => props.opacity};
+  /* right: ${props => props.trigger1}; */
+  /* transition: right 1s; */
+  max-width: 1000px;
+  margin-right: 1.6rem;
+  margin-left: 1.6rem;
+  font-size: 70px;
+  line-height: 86px;
+  @media screen and (max-width: 479px) {
+    font-size: 50px;
+    line-height: 54px;
+  }
+`;
+
+const Trigger = styled.div`
+  ${Heading}
+  ${HeadingHero}
+`;
+
+interface Props {}
+interface State {
+  opacity: string;
+  top: string;
+}
+
+export class HeroText extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      opacity: "1",
+      top: "30px",
+    };
+  }
+
+  render(): React.ReactNode {
+    const Element = (
+      <Trigger
+        id="hero-text"
+        key={1}
+        opacity={this.state.opacity}
+        top={this.state.top}
+      >
+        Goodbye, big tech.
+        <br />
+        Welcome, <span id="text-span">your data at your own home.</span>
+        <span id="text-span"></span>
+        <span id="text-span"></span>
+      </Trigger>
+    );
+    console.log(Element.props);
+
+    return (
+      <>
+        <Loader
+          triggerkey="#hero-text"
+          elements={[Element]}
+          cb={this.cb}
+          threshold={0}
+        ></Loader>
+      </>
+    );
+  }
+  cb = (entry: IntersectionObserverEntry) => {
+    this.setAttributes({ opacity: "1", top: "0" });
+  };
+  setAttributes = ({ opacity, top }: State) => this.setState({ opacity, top });
+}
