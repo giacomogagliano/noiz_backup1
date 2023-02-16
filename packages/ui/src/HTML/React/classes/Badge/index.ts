f142d392f829077ce6acd7bc3c9d03bc15e60e6e
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-02-16 18:09:05 +0100

    rexport from components folder

--- packages/ui/src/HTML/React/classes/Badge/index.ts
+++ packages/ui/src/HTML/React/classes/Badge/index.ts
@@ -1,15 +1,8 @@
 export {
-  Badge_v2 as Badge,
+  Badge_v2,
   Badge_v2Props as BadgeProps,
   Badge_v2State as BadgeState,
 } from "./Badge_v2";
 export type {} from "./Badge_v2";
 
-// export { Badge_v1 } from "./Badge_v1";
-// export type {
-//   Badge_v1Data as BadgeData,
-//   Badge_v1Booleans as BadgeBooleans,
-//   Badge_v1Props as BadgeProps,
-//   Badge_v1ClassProps as BadgeClassProps,
-//   Badge_v1AsChild as BadgeAsChild,
-// } from "./Badge_v1";
+export { Badge } from "../../components";
