6ae422f86795ef62f7e3bde12f2c59148279ab71
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-02-16 09:45:02 +0100

    move Scroller

--- apps/test-next-13/src/app/page.tsx
+++ apps/test-next-13/src/app/page.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import Client from "../classes/Client";
 import { Header } from "../components/Header";
-import Scroller from "../classes/Scroller";
+import Scroller from "../lib/client/Scroller";
 import { FullPage } from "../components/FullPage";
 export default function page() {
   return (
