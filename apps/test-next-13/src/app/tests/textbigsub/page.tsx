d3afd2bd6a09ec0aa3d542b9cff5db2fe830ac4d
ariannatnl <arianna.minucci@me.com>
2023-02-21 15:28:56 +0100

    test

--- apps/test-next-13/src/app/tests/textbigsub/page.tsx
+++ apps/test-next-13/src/app/tests/textbigsub/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { GradientButton } from "../../../classes/GradientButton";
+import { NewLoader } from "../../../lib/client/NewLoader";
 
 export default function page() {
   return (
@@ -10,32 +10,55 @@ export default function page() {
         color: "white",
       }}
     >
-      <p style={{ margin: 0 }}>ciao</p>
-      <p style={{ margin: 0 }}>ciao</p>
-      <p style={{ margin: 0 }}>ciao</p>
-      <p style={{ margin: 0 }}>ciao</p>
-      <p style={{ margin: 0 }}>ciao</p>
-      <p style={{ margin: 0 }}>ciao</p>
-      <p style={{ margin: 0 }}>ciao</p>
-      <p style={{ margin: 0 }}>ciao</p>
-      <p style={{ margin: 0 }}>ciao</p>
-      <p style={{ margin: 0 }}>ciao</p>
-      <p style={{ margin: 0 }}>ciao</p>
-      <p style={{ margin: 0 }}>ciao</p>
-      <p style={{ margin: 0 }}>ciao</p>
-      <p style={{ margin: 0 }}>ciao</p>
-      <p style={{ margin: 0 }}>ciao</p>
-      <p style={{ margin: 0 }}>ciao</p>
-      <p style={{ margin: 0 }}>ciao</p>
-      <p style={{ margin: 0 }}>ciao</p>
-      <p style={{ margin: 0 }}>ciao</p>
-      <p style={{ margin: 0 }}>ciao</p>
-      <p style={{ margin: 0 }}>ciao</p>
-      <p style={{ margin: 0 }}>ciao</p>
-      <p style={{ margin: 0 }}>ciao</p>
-      <p style={{ margin: 0 }}>ciao</p>
-      <p style={{ margin: 0 }}>ciao</p>
-      <GradientButton></GradientButton>
+      <h1>lolol</h1>
+      <h1>lolol</h1>
+      <h1>lolol</h1>
+      <h1>lolol</h1>
+      <h1>lolol</h1>
+      <h1>lolol</h1>
+      <h1>lolol</h1>
+      <h1>lolol</h1>
+      <h1>lolol</h1>
+      <h1>lolol</h1>
+      <h1>lolol</h1>
+      <h1>lolol</h1>
+      <h1>lolol</h1>
+      <h1>lolol</h1>
+      <h1>lolol</h1>
+      <h1>lolol</h1>
+      <h1>lolol</h1>
+      <h1>lolol</h1>
+      <h1>lolol</h1>
+      <h1>lolol</h1>
+      <h1>lolol</h1>
+      <h1>lolol</h1>
+      <h1>lolol</h1>
+      <h1>lolol</h1>
+      <h1>lolol</h1>
+
+      <NewLoader
+        stateB="opacity: 0; transform: translateY(40px); transition: transform 400ms ease-in-out, opacity 400ms ease-out;
+        "
+        stateA="opacity: 1; transform: translateY(0); transition: transform 400ms ease-in-out, opacity 400ms ease-out;
+        "
+        triggerKey="#text-big-sub"
+        threshold={1}
+      >
+        <h2 id="text-big-sub">
+          Welcome to the <span>new Era of internet.</span>
+        </h2>
+        <h3 id="text-big-sub">
+          One that's powered by you. <span></span>
+        </h3>
+      </NewLoader>
+      <h1>lolol</h1>
+      <h1>lolol</h1>
+      <h1>lolol</h1>
+      <h1>lolol</h1>
+      <h1>lolol</h1>
+      <h1>lolol</h1>
+      <h1>lolol</h1>
+      <h1>lolol</h1>
     </div>
   );
 }
