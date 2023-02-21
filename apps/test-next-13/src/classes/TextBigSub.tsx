91d5dae83a3e777c71fea56d5fbabaf5170adefd
ariannatnl <arianna.minucci@me.com>
2023-02-21 15:29:31 +0100

    edited textBigSub new loader added

--- apps/test-next-13/src/classes/TextBigSub.tsx
+++ apps/test-next-13/src/classes/TextBigSub.tsx
@@ -3,40 +3,41 @@ import React, { Component } from "react";
 import styled, { css } from "styled-components";
 
 import { Loader } from "../lib/client/Loader";
+import { NewLoader } from "../lib/client/NewLoader";
 import { TextSpan2 } from "../lib/style/Section.style";
 import { BigHeading, SubHeading } from "../lib/style/StylesSheet";
 
-interface TextBigSubProps {
-  parallaxSharedY: string;
-}
+// interface TextBigSubProps {
+//   parallaxSharedY: string;
+// }
 
-type TextBigSubState = {
-  opacity: string;
-  opacity2: string;
-  transform: string;
-  transform2: string;
-};
+// type TextBigSubState = {
+//   opacity: string;
+//   opacity2: string;
+//   transform: string;
+//   transform2: string;
+// };
 
-const Trigger = styled.div<Pick<TextBigSubState, "opacity" | "transform">>`
-  #big-heading {
-    ${BigHeading}
-    opacity: ${props => props.opacity};
-    transform: translateY(${props => props.transform});
-    transition: transform 400ms ease-in-out, opacity 400ms ease-out;
-    #text-span- {
-      ${TextSpan2}
-    }
-  }
-  #SubsHeading {
-    ${SubHeading}
-    opacity: ${props => props.opacity};
-    transform: translateY(${props => props.transform});
-    transition: transform 400ms ease-in-out, opacity 400ms ease-out;
-    #text-span- {
-      ${TextSpan2}
-    }
-  }
-`;
+// const Trigger = styled.div<Pick<TextBigSubState, "opacity" | "transform">>`
+//   #big-heading {
+//     ${BigHeading}
+//     opacity: ${props => props.opacity};
+//     transform: translateY(${props => props.transform});
+//     transition: transform 400ms ease-in-out, opacity 400ms ease-out;
+//     #text-span- {
+//       ${TextSpan2}
+//     }
+//   }
+//   #SubsHeading {
+//     ${SubHeading}
+//     opacity: ${props => props.opacity};
+//     transform: translateY(${props => props.transform});
+//     transition: transform 400ms ease-in-out, opacity 400ms ease-out;
+//     #text-span- {
+//       ${TextSpan2}
+//     }
+//   }
+// `;
 
 export class TextBigSub extends Component<TextBigSubProps, TextBigSubState> {
   constructor(props: TextBigSubProps) {
@@ -51,46 +52,56 @@ export class TextBigSub extends Component<TextBigSubProps, TextBigSubState> {
   }
 
   render(): React.ReactNode {
-    const Element = (
-      <Trigger
-        key={0}
-        id="text-big-sub"
-        opacity={this.state.opacity}
-        transform={this.state.transform}
-      >
-        <h2 id="big-heading">
-          Welcome to the <span id="text-span-">new Era of internet.</span>
-        </h2>
-      </Trigger>
-    );
-    const Element2 = (
-      <Trigger
-        key={1}
-        id="text-big-sub2"
-        opacity={this.state.opacity2}
-        transform={this.state.transform2}
-      >
-        <h3 id="SubsHeading">
-          One that's powered by you. <span id="text-span-"></span>
-        </h3>
-      </Trigger>
-    );
+    // const Element = (
+    //   <Trigger
+    //     key={0}
+    //     id="text-big-sub"
+    //     opacity={this.state.opacity}
+    //     transform={this.state.transform}
+    //   >
+    //     <h2 id="big-heading">
+    //       Welcome to the <span id="text-span-">new Era of internet.</span>
+    //     </h2>
+    //   </Trigger>
+    // );
+    // const Element2 = (
+    //   <Trigger
+    //     key={1}
+    //     id="text-big-sub2"
+    //     opacity={this.state.opacity2}
+    //     transform={this.state.transform2}
+    //   >
+    //     <h3 id="SubsHeading">
+    //       One that's powered by you. <span id="text-span-"></span>
+    //     </h3>
+    //   </Trigger>
+    // );
     return (
       <>
-        <Loader
-          triggerkey="#text-big-sub"
-          elements={[Element]}
-          cb={this.cb}
+        <NewLoader
+          stateB="opacity: 0; transform: translateY(40px); transition: transform 400ms ease-in-out, opacity 400ms ease-out;
+        "
+          stateA="opacity: 1; transform: translateY(0); transition: transform 400ms ease-in-out, opacity 400ms ease-out;
+        "
+          triggerKey="#big-heading"
           threshold={1}
-          rootMargin={"-20px"}
-        ></Loader>
-        <Loader
-          triggerkey="#text-big-sub2"
-          elements={[Element2]}
-          cb={this.cb2}
-          threshold={0.6}
-          rootMargin={"-90px"}
-        ></Loader>
+        >
+          <h2 id="big-heading">
+            Welcome to the <span id="text-span-">new Era of internet.</span>
+          </h2>
+        </NewLoader>
+        <NewLoader
+          stateB="opacity: 0; transform: translateY(40px); transition: transform 400ms ease-in-out, opacity 400ms ease-out;
+          "
+          stateA="opacity: 1; transform: translateY(0); transition: transform 400ms ease-in-out, opacity 400ms ease-out;
+          "
+          triggerKey="#SubsHeading"
+          threshold={1}
+        >
+          <h3 id="SubsHeading">
+            One that's powered by you. <span id="text-span-"></span>
+          </h3>
+        </NewLoader>
       </>
     );
   }
