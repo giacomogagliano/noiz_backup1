e616f89545e5a82bd68cc05e19739758baca591a
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-02-21 12:59:13 +0100

    add

---
+++ apps/test-next-13/src/app/tests/new-loader/page.tsx
@@ -0,0 +1,72 @@
+import React, { FunctionComponent } from "react";
+import { stateA, stateB, Test } from "../../../lib/style/Test.style";
+import dynamic from "next/dynamic";
+
+function dyn(loading: FunctionComponent) {
+  return dynamic(
+    () => import("../../../lib/client/NewLoader").then(m => m.NewLoader),
+    { ssr: false, loading }
+  );
+}
+
+let NewLoader1 = dyn(() => <Test id="test-trigger">ciao</Test>);
+let NewLoader2 = dyn(() => <Test id="test-trigger">addio</Test>);
+
+export default function page() {
+  return (
+    <>
+      <div>ciao</div>
+      <NewLoader1
+        triggerKey="#test-trigger"
+        stateA={stateA as unknown as string}
+        stateB={stateB as unknown as string}
+        threshold={1}
+      >
+        <Test id="test-trigger">ciao</Test>
+        <Test id="test-trigger">ciao</Test>
+        <Test id="test-trigger">ciao</Test>
+        <Test id="test-trigger">ciao</Test>
+        <Test id="test-trigger">ciao</Test>
+        <Test id="test-trigger">ciao</Test>
+        <Test id="test-trigger">ciao</Test>
+        <Test id="test-trigger">ciao</Test>
+        <Test id="test-trigger">ciao</Test>
+        <Test id="test-trigger">ciao</Test>
+        <Test id="test-trigger">ciao</Test>
+        <Test id="test-trigger">ciao</Test>
+        <Test id="test-trigger">ciao</Test>
+        <Test id="test-trigger">ciao</Test>
+        <Test id="test-trigger">ciao</Test>
+        <Test id="test-trigger">ciao</Test>
+        <Test id="test-trigger">ciao</Test>
+        <Test id="test-trigger">ciao</Test>
+        <Test id="test-trigger">ciao</Test>
+        <Test id="test-trigger">ciao</Test>
+        <Test id="test-trigger">ciao</Test>
+        <Test id="test-trigger">ciao</Test>
+        <Test id="test-trigger">ciao</Test>
+        <Test id="test-trigger">ciao</Test>
+        <Test id="test-trigger">ciao</Test>
+        <Test id="test-trigger">ciao</Test>
+        <Test id="test-trigger">ciao</Test>
+        <Test id="test-trigger">ciao</Test>
+        <Test id="test-trigger">ciao</Test>
+        <Test id="test-trigger">ciao</Test>
+        <Test id="test-trigger">ciao</Test>
+        <Test id="test-trigger">ciao</Test>
+        <Test id="test-trigger">ciao</Test>
+        <Test id="test-trigger">ciao</Test>
+        <Test id="test-trigger">ciao</Test>
+      </NewLoader1>
+      <div>no effect</div>
+      <div>no effect</div>
+      <NewLoader2
+        triggerKey="#test-2"
+        stateA="color:blue; transition: 3s;"
+        stateB="color: green;"
+      >
+        <div id="test-2">other effect</div>
+      </NewLoader2>
+    </>
+  );
+}
