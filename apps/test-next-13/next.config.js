4cf5b0e7e16f1f4018088b23039fbd554504593f
ariannatnl <arianna.minucci@me.com>
2023-02-22 10:29:44 +0100

    added import themeProvider

--- apps/test-next-13/next.config.js
+++ apps/test-next-13/next.config.js
@@ -2,6 +2,12 @@ module.exports = {
   webpack: config => {
     config.resolve.fallback = {
       "styled-components": false,
+      fs: false,
+      "rehype-highlight": false,
+      "rehype-stringify": false,
+      "remark-parse": false,
+      "emark-rehype": false,
+      "gray-matter": false,
     };
     return config;
   },
--- apps/test-next-13/src/app/layout.tsx
+++ apps/test-next-13/src/app/layout.tsx
@@ -1,5 +1,7 @@
+"use client";
 import { Body } from "../lib/style/Body.style";
 import StyledComponentsRegistry from "../lib/registry";
+import ThemeProvider from "../classes/ThemeProvider";
 
 export default function RootLayout({
   children,
@@ -10,7 +12,9 @@ export default function RootLayout({
     <html>
       <head />
       <StyledComponentsRegistry>
-        <Body>{children}</Body>
+        <ThemeProvider>
+          <Body>{children}</Body>
+        </ThemeProvider>
       </StyledComponentsRegistry>
     </html>
   );
