bd70de98cbb76246fb659660216eb514f5f673b3
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-02-23 10:11:15 +0100

    add root in arguments, edit comment, refactor ifs

--- apps/test-next-13/src/lib/client/Loader.tsx
+++ apps/test-next-13/src/lib/client/Loader.tsx
@@ -1,16 +1,64 @@
 "use client";
 import { Component, ReactNode, use } from "react";
+import { FlattenSimpleInterpolation } from "styled-components";
+
 type Props = {
   children: ReactNode;
   triggerKey: string;
-  stateA: string;
-  stateB: string;
+  stateA: FlattenSimpleInterpolation;
+  stateB: FlattenSimpleInterpolation;
   threshold?: number;
   rootMargin?: string;
   callBack?: "cb" | "cb2";
+  root?: Document | Element;
 };
 
-class NewLoader_v1 extends Component<Props, {}> {
+interface LoaderProcessorProps {
+  entry: IntersectionObserverEntry;
+  setValue: (value: string) => void;
+  stateA: string;
+  stateB: string;
+}
+
+interface LoaderProcessor {
+  (props: LoaderProcessorProps): void;
+}
+/**
+ * Questa classe crea degli elementi mutabili, le quali
+ * mutazione è fatta scatenare da un elemento trigger.
+ * La mutazione è la stessa per tutti gli elementi da
+ * mutare.
+ *
+ * ### Elementi Mutabili
+ *
+ * Gli elementi mutabili vengono passati al Loader come
+ * children e devono essere composti in modo tale che
+ * l'elemento piu esterno abbia un id equivalente al valore
+ * parametro `triggerKey`.
+ *
+ * ### Strategie
+ *
+ * Questa classe può essere usata con 3 strategie:
+ *
+ * - cb_1: la mutazione avviene ogni volta che l'elemento
+ *   interseziona il root
+ * - cb_2: la mutazione avviene solo quando l'elemento
+ *   interseziona il root quando il valore dell'intersezione
+ *   con l'asse Y è diverso da zero (ovvero quando
+ *   interseziona la parte inferiore del root)
+ *
+ * @param children Sono gli elementi che verranno mutati
+ * @param triggerKey
+ * @param children
+ * @param triggerKey
+ * @param stateA
+ * @param stateB
+ * @param threshold
+ * @param rootMargin
+ * @param callBack
+ * @param root
+ */
+class Loader_v1 extends Component<Props, {}> {
   render(): ReactNode {
     return <>{this.props.children}</>;
   }
@@ -21,9 +69,11 @@ class NewLoader_v1 extends Component<Props, {}> {
     const elements = document.querySelectorAll(this.props.triggerKey);
     let threshold = this.props.threshold;
     let rootMargin = this.props.rootMargin;
+    let root = this.props.root;
     let observer = new IntersectionObserver(this.observerCb, {
       threshold,
       rootMargin,
+      root,
     });
     elements.forEach(this.observeElement(observer));
   };
@@ -37,17 +87,18 @@ class NewLoader_v1 extends Component<Props, {}> {
     function setValue(value: string) {
       entry.target.setAttribute("style", value);
     }
-    if (this.props.callBack === "cb") {
-      if (entry.isIntersecting)
-        setValue(this.props.stateA as unknown as string);
-      else setValue(this.props.stateB as unknown as string);
-    } else {
-      if (entry.intersectionRect.y) {
-        if (entry.isIntersecting)
-          setValue(this.props.stateA as unknown as string);
-        else setValue(this.props.stateB as unknown as string);
-      }
-    }
+    let stateA: string = this.props.stateA as unknown as string;
+    let stateB: string = this.props.stateB as unknown as string;
+    let cb1 = this.props.callBack === "cb";
+    if (cb1) this.cb_1({ entry, setValue, stateA, stateB });
+    else this.cb_2({ entry, setValue, stateA, stateB });
+  };
+  cb_1: LoaderProcessor = ({ entry, setValue, stateA, stateB }) => {
+    if (entry.isIntersecting) setValue(stateA);
+    else setValue(stateB);
+  };
+  cb_2: LoaderProcessor = props => {
+    if (props.entry.intersectionRect.y) this.cb_1(props);
   };
 
   triggerKey: string;
@@ -57,4 +108,4 @@ class NewLoader_v1 extends Component<Props, {}> {
   stateB: string;
 }
 
-export const Loader = NewLoader_v1;
+export const Loader = Loader_v1;
