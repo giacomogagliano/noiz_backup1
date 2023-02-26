631a6cac079612e1896f92d7a40e65d7d76a4825
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-02-26 20:51:02 +0100

    exports types

--- packages/zionbase/src/zionbase/index.ts
+++ packages/zionbase/src/zionbase/index.ts
@@ -1,6 +1,6 @@
 export * as Class from "./Class";
 export * as DataStructure from "./DataStructures";
-// export * as Types from "./Types";
+export * as Types from "./Types";
 
 // // Mixin
 // export * as Mixin from "./Mixin";
