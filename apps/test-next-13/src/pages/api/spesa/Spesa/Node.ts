7212d5a7924752de921a3f82b6f13507e13f4aae
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-02-26 21:04:03 +0100

    imports Node from zionbase

--- apps/test-next-13/src/pages/api/spesa/Spesa/Node.ts
+++ apps/test-next-13/src/pages/api/spesa/Spesa/Node.ts
@@ -1,7 +1,7 @@
-export interface Node<T> {
-  type: string;
-  value: T;
-}
+import type { Types } from "@zionstate/zionbase/zionbase";
+
+export interface Node<T> extends Types.DataStructure.Graph.BasicNode<T> {}
+
 export abstract class Node<T> implements Node<T> {
   value: T;
   constructor(p: T) {
