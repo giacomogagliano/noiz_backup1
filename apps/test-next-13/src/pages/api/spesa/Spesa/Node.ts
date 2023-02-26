ac96945353784deec70b0ca7a5c331cd9ca82c93
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-02-26 21:27:20 +0100

    deletes useless import

--- apps/test-next-13/src/pages/api/spesa/Spesa/Node.ts
+++ apps/test-next-13/src/pages/api/spesa/Spesa/Node.ts
@@ -1,5 +1,4 @@
 import { Types } from "@zionstate/zionbase/zionbase";
-import { Types as Abstracts } from "@zionstate/zionbase/zionbase";
 
 export interface Node<T> extends Types.DataStructure.Graph.BasicNode<T> {}
 
